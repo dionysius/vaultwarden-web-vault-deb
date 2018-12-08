6c22919bd983d4a489ec558faaa5876076b1d93a
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-12-08 14:00:18 -0500

    colorized passwords

--- src/popup/app.module.ts
+++ src/popup/app.module.ts
@@ -54,6 +54,7 @@ import { StopClickDirective } from 'jslib/angular/directives/stop-click.directiv
 import { StopPropDirective } from 'jslib/angular/directives/stop-prop.directive';
 import { TrueFalseValueDirective } from 'jslib/angular/directives/true-false-value.directive';
 
+import { ColorPasswordPipe } from 'jslib/angular/pipes/color-password.pipe';
 import { I18nPipe } from 'jslib/angular/pipes/i18n.pipe';
 import { SearchCiphersPipe } from 'jslib/angular/pipes/search-ciphers.pipe';
 
@@ -156,6 +157,7 @@ registerLocaleData(localeZhTw, 'zh-TW');
         CiphersComponent,
         CiphersListComponent,
         CollectionsComponent,
+        ColorPasswordPipe,
         CurrentTabComponent,
         EnvironmentComponent,
         ExportComponent,
