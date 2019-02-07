2a129c0e52e7ebe10d45b0b3a3bb39203cc1cfb0
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-02-07 16:16:45 -0500

    add billing and mail address to autofill detection

--- src/services/autofill.service.ts
+++ src/services/autofill.service.ts
@@ -654,8 +654,8 @@ export default class AutofillService implements AutofillServiceInterface {
                     fillFields.email = f;
                     break;
                 } else if (!fillFields.address && this.isFieldMatch(f[attr],
-                    ['address', 'street-address', 'addr', 'street', 'mailing-address', 'billing-address',
-                        'mail-address', 'bill-address'], [])) {
+                    ['address', 'street-address', 'addr', 'street', 'mailing-addr', 'billing-addr',
+                        'mail-addr', 'bill-addr'], ['mailing-addr', 'billing-addr', 'mail-addr', 'bill-addr'])) {
                     fillFields.address = f;
                     break;
                 } else if (!fillFields.address1 && this.isFieldMatch(f[attr],
