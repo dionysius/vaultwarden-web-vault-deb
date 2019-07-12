a30b941a1dd37653d3af6d44e40ca772f9cca831
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-07-12 17:11:43 -0400

    export vault event

--- src/popup/settings/export.component.ts
+++ src/popup/settings/export.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { CryptoService } from 'jslib/abstractions/crypto.service';
+import { EventService } from 'jslib/abstractions/event.service';
 import { ExportService } from 'jslib/abstractions/export.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
@@ -15,8 +16,8 @@ import { ExportComponent as BaseExportComponent } from 'jslib/angular/components
 export class ExportComponent extends BaseExportComponent {
     constructor(cryptoService: CryptoService, i18nService: I18nService,
         platformUtilsService: PlatformUtilsService, exportService: ExportService,
-        private router: Router) {
-        super(cryptoService, i18nService, platformUtilsService, exportService, window);
+        eventService: EventService, private router: Router) {
+        super(cryptoService, i18nService, platformUtilsService, exportService, eventService, window);
     }
 
     protected saved() {
