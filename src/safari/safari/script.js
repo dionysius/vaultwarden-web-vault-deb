4be51b6bc8cddcc87b8bd4cb04a3d33cff68d01b
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-08-22 10:29:55 -0400

    remove old script.js

--- src/safari/safari/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-                          safari.extension.dispatchMessage("bitwarden", { hello: "world!!!" });
-                          
-                          safari.self.addEventListener("message", (e) => {
-                                                       console.log(e);
-                                                       
-                                                       });
-});
