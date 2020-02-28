a06ef80dc68ba1bfdb0e53bd14ca287f0812fd8c
Kyle Spearrin <kspearrin@users.noreply.github.com>
2020-02-28 16:58:28 -0500

    implement callout component w/ generator policy (#1158)

--- src/popup/app.module.ts
+++ src/popup/app.module.ts
@@ -65,6 +65,7 @@ import { ActionButtonsComponent } from './components/action-buttons.component';
 import { CiphersListComponent } from './components/ciphers-list.component';
 import { PopOutComponent } from './components/pop-out.component';
 
+import { CalloutComponent } from 'jslib/angular/components/callout.component';
 import { IconComponent } from 'jslib/angular/components/icon.component';
 
 import {
@@ -165,6 +166,7 @@ registerLocaleData(localeZhTw, 'zh-TW');
         AutofocusDirective,
         BlurClickDirective,
         BoxRowDirective,
+        CalloutComponent,
         CiphersComponent,
         CiphersListComponent,
         CollectionsComponent,
