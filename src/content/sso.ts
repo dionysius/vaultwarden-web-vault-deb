033d02d3ec620337d15e4d9e495e00a33122be1e
Matt Smith <msmith@bitwarden.com>
2020-08-25 10:19:39 -0500

    Linter Issues / JSLib Update

--- src/content/sso.ts
+++ src/content/sso.ts
@@ -1,8 +1,8 @@
-window.addEventListener("message", function(event) {
-    if (event.source != window)
+window.addEventListener('message', (event) => {
+    if (event.source !== window)
         return;
 
-    if (event.data.command && (event.data.command == "authResult")) {
+    if (event.data.command && (event.data.command === 'authResult')) {
         chrome.runtime.sendMessage({
             command: event.data.command,
             code: event.data.code,
