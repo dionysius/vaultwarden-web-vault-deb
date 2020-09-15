382651cc8d3d5c833d7dc06d340b998937c1cbde
Chad Scharf <3904944+cscharf@users.noreply.github.com>
2020-09-15 11:04:32 -0400

    removed old edge shortcut key(s)

--- src/content/shortcuts.ts
+++ src/content/shortcuts.ts
@@ -3,10 +3,9 @@ import * as Mousetrap from 'mousetrap';
 document.addEventListener('DOMContentLoaded', (event) => {
     const isSafari = (typeof safari !== 'undefined') && navigator.userAgent.indexOf(' Safari/') !== -1 &&
         navigator.userAgent.indexOf('Chrome') === -1;
-    const isEdge = !isSafari && navigator.userAgent.indexOf(' Edg/') !== -1;
     const isVivaldi = !isSafari && navigator.userAgent.indexOf(' Vivaldi/') !== -1;
 
-    if (!isSafari && !isEdge && !isVivaldi) {
+    if (!isSafari && !isVivaldi) {
         return;
     }
 
@@ -21,8 +20,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let autofillCommand = ['mod+shift+l'];
     if (isSafari) {
         autofillCommand = ['mod+\\', 'mod+8', 'mod+shift+p'];
-    } else if (isEdge) {
-        autofillCommand = ['mod+\\', 'mod+9'];
     }
     Mousetrap.bind(autofillCommand, () => {
         sendMessage('autofill_login');
