296ccb68298e7f1de591a16adce59b99cc40152a
Hinton <hinton.oscar@gmail.com>
2020-10-05 15:44:19 +0200

    WIP desktop communication

--- src/background.ts
+++ src/background.ts
@@ -4,3 +4,13 @@ const bitwardenMain = (window as any).bitwardenMain = new MainBackground();
 bitwardenMain.bootstrap().then(() => {
     // Finished bootstrapping
 });
+
+const port = chrome.runtime.connectNative('com.8bit.bitwarden');
+
+port.onMessage.addListener((msg: any) => {
+    console.log('Received' + msg);
+});
+port.onDisconnect.addListener(() => {
+    console.log('Disconnected');
+});
+port.postMessage({ text: 'Hello, my_application' });
