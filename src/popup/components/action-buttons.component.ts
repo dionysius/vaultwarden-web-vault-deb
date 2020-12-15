29c624e37b9e7d9f46625b239b235841da241c4e
Matt Gibson <mgibson@bitwarden.com>
2020-12-15 10:26:01 -0600

    Add totp copy to clipboard button to cipher view (#1493)
    * Add totp copy to clipboard button to cipher view
    
    * Hide quick-copy rather than disable if no totp
    
    * Revert to disabled TOTP button
    
    * Enforce premium access to TOTPs
    
    * Update jslib reference

--- src/popup/components/action-buttons.component.ts
+++ src/popup/components/action-buttons.component.ts
@@ -18,6 +18,8 @@ import { CipherView } from 'jslib/models/view/cipherView';
 import { EventService } from 'jslib/abstractions/event.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
+import { TotpService } from 'jslib/abstractions/totp.service';
+import { UserService } from 'jslib/abstractions/user.service';
 
 import { PopupUtilsService } from '../services/popup-utils.service';
 
@@ -31,11 +33,17 @@ export class ActionButtonsComponent {
     @Input() showView = false;
 
     cipherType = CipherType;
+    userHasPremiumAccess = false;
 
     constructor(private analytics: Angulartics2, private toasterService: ToasterService,
         private i18nService: I18nService, private platformUtilsService: PlatformUtilsService,
-        private popupUtilsService: PopupUtilsService, private eventService: EventService) { }
+        private popupUtilsService: PopupUtilsService, private eventService: EventService,
+        private totpService: TotpService, private userService: UserService) { }
 
+    async ngOnInit() {
+        this.userHasPremiumAccess = await this.userService.canAccessPremium();
+    }
+    
     launch() {
         if (this.cipher.type !== CipherType.Login || !this.cipher.login.canLaunch) {
             return;
@@ -48,9 +56,11 @@ export class ActionButtonsComponent {
         }
     }
 
-    copy(cipher: CipherView, value: string, typeI18nKey: string, aType: string) {
-        if (value == null) {
+    async copy(cipher: CipherView, value: string, typeI18nKey: string, aType: string) {
+        if (value == null || !this.displayTotpCopyButton(cipher)) {
             return;
+        } else if (value === cipher.login.totp) {
+            value = await this.totpService.getCode(value);
         }
 
         if (!cipher.viewPassword) {
@@ -62,11 +72,16 @@ export class ActionButtonsComponent {
         this.toasterService.popAsync('info', null,
             this.i18nService.t('valueCopied', this.i18nService.t(typeI18nKey)));
 
-        if (typeI18nKey === 'password') {
+        if (typeI18nKey === 'password' || typeI18nKey === 'verificationCodeTotp') {
             this.eventService.collect(EventType.Cipher_ClientToggledHiddenFieldVisible, cipher.id);
         } else if (typeI18nKey === 'securityCode') {
             this.eventService.collect(EventType.Cipher_ClientCopiedCardCode, cipher.id);
         }
+    }
+
+    displayTotpCopyButton(cipher: CipherView) {
+        return (cipher?.login?.hasTotp ?? false) &&
+            (cipher.organizationUseTotp || this.userHasPremiumAccess);
     }
 
     view() {
