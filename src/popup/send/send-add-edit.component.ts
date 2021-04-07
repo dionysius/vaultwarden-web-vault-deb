f68858e729814fc9aed8d774f79cc4a7505ca67b
Thomas Rittson <trittson@bitwarden.com>
2021-04-07 12:49:06 +1000

    Require user to verify email to use file Send

--- src/popup/send/send-add-edit.component.ts
+++ src/popup/send/send-add-edit.component.ts
@@ -16,6 +16,7 @@ import { MessagingService } from 'jslib/abstractions/messaging.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { PolicyService } from 'jslib/abstractions/policy.service';
 import { SendService } from 'jslib/abstractions/send.service';
+import { TokenService } from 'jslib/abstractions/token.service';
 import { UserService } from 'jslib/abstractions/user.service';
 
 import { PopupUtilsService } from '../services/popup-utils.service';
@@ -39,9 +40,9 @@ export class SendAddEditComponent extends BaseAddEditComponent {
         userService: UserService, messagingService: MessagingService, policyService: PolicyService,
         environmentService: EnvironmentService, datePipe: DatePipe, sendService: SendService,
         private route: ActivatedRoute, private router: Router, private location: Location,
-        private popupUtilsService: PopupUtilsService) {
+        private popupUtilsService: PopupUtilsService, private tokenService: TokenService) {
         super(i18nService, platformUtilsService, environmentService, datePipe, sendService, userService,
-            messagingService, policyService);
+            messagingService, policyService, tokenService);
     }
 
     get showFileSelector(): boolean {
