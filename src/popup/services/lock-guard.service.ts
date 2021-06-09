ebba6aed7ff7e8fb22d127237a06f82443c9ff74
Thomas Rittson <trittson@bitwarden.com>
2021-06-10 07:36:30 +1000

    Remove unnecessary constructors

--- src/popup/services/lock-guard.service.ts
+++ src/popup/services/lock-guard.service.ts
@@ -1,16 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 
-import { UserService } from 'jslib-common/abstractions/user.service';
-import { VaultTimeoutService } from 'jslib-common/abstractions/vaultTimeout.service';
 import { LockGuardService as BaseLockGuardService } from 'jslib-angular/services/lock-guard.service';
 
 @Injectable()
 export class LockGuardService extends BaseLockGuardService {
-    constructor(vaultTimeoutService: VaultTimeoutService, userService: UserService,
-        router: Router) { 
-        super(vaultTimeoutService, userService, router);
-        }
-
     protected homepage = 'tabs/current';
 }
--- src/popup/services/unauth-guard.service.ts
+++ src/popup/services/unauth-guard.service.ts
@@ -1,19 +1,8 @@
 import { Injectable } from '@angular/core';
-import {
-    Router,
-} from '@angular/router';
-
-import { UserService } from 'jslib-common/abstractions/user.service';
-import { VaultTimeoutService } from 'jslib-common/abstractions/vaultTimeout.service';
 
 import { UnauthGuardService as BaseUnauthGuardService } from 'jslib-angular/services/unauth-guard.service';
 
 @Injectable()
 export class UnauthGuardService extends BaseUnauthGuardService {
-    constructor(vaultTimeoutService: VaultTimeoutService, userService: UserService,
-        router: Router) {
-        super(vaultTimeoutService, userService, router);
-        }
-
     protected homepage = 'tabs/current';
 }
