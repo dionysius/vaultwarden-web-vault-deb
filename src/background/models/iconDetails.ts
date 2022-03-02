7e432b40e24bd67b3536618eed569094a72be251
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-03-02 16:12:01 -0500

    details, details

--- src/background/models/iconDetails.ts
+++ src/background/models/iconDetails.ts
@@ -3,6 +3,6 @@ export default class IconDetails {
     19: string;
     38: string;
   };
-  // Chrome does not support windowId, only FireFox
+  // Chrome does not support windowId, only Firefox
   windowId?: number;
 }
