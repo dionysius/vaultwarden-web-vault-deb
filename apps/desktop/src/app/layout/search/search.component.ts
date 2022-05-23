60b39bc211e79c02ca53305eee4874fd20ed5531
CarleyDiaz-Bitwarden <103955722+CarleyDiaz-Bitwarden@users.noreply.github.com>
2022-05-23 14:04:19 -0400

    Clearing the search bar when switching accounts (#2685)
    * Clearing the search bar when switching accounts
    
    * Fixing Lint Issues and Prettier
    
    * Updating the message to use switchAccount instead of a new message name "clearSearchBarText"
    
    * Updating to use Observable on activeAccount
    
    * adding back line
    
    * adding back line
    
    * Adding OnInit OnDestroy

--- apps/desktop/src/app/layout/search/search.component.ts
+++ apps/desktop/src/app/layout/search/search.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
+
+import { StateService } from "jslib-common/abstractions/state.service";
 
 import { SearchBarService, SearchBarState } from "./search-bar.service";
 
@@ -7,11 +9,11 @@ import { SearchBarService, SearchBarState } from "./search-bar.service";
   selector: "app-search",
   templateUrl: "search.component.html",
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit, OnDestroy {
   state: SearchBarState;
   searchText: FormControl = new FormControl(null);
 
-  constructor(private searchBarService: SearchBarService) {
+  constructor(private searchBarService: SearchBarService, private stateService: StateService) {
     this.searchBarService.state.subscribe((state) => {
       this.state = state;
     });
@@ -19,5 +21,16 @@ export class SearchComponent {
     this.searchText.valueChanges.subscribe((value) => {
       this.searchBarService.setSearchText(value);
     });
+  }
+
+  ngOnInit() {
+    this.stateService.activeAccount.subscribe((value) => {
+      this.searchBarService.setSearchText("");
+      this.searchText.patchValue("");
+    });
+  }
+
+  ngOnDestroy() {
+    this.stateService.activeAccount.unsubscribe();
   }
 }
