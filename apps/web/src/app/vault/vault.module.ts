7e39867dae390d2b7d40f41b33d92d82477761e9
Addison Beck <addisonbeck1@gmail.com>
2022-08-16 09:10:35 -0400

    Import OrganizationBadgeModule in VaultModule (#3316)

--- apps/web/src/app/vault/vault.module.ts
+++ apps/web/src/app/vault/vault.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 
 import { CiphersComponent } from "./ciphers.component";
+import { OrganizationBadgeModule } from "./organization-badge/organization-badge.module";
 import { VaultSharedModule } from "./shared/vault-shared.module";
 import { VaultRoutingModule } from "./vault-routing.module";
 import { VaultComponent } from "./vault.component";
 
 @NgModule({
-  imports: [VaultSharedModule, VaultRoutingModule],
+  imports: [VaultSharedModule, VaultRoutingModule, OrganizationBadgeModule],
   declarations: [VaultComponent, CiphersComponent],
   exports: [VaultComponent],
 })
