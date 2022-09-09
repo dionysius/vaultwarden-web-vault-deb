9af92fd6266dc290613980b999d50e9f048655dc
Patrick H. Lauke <redux@splintered.co.uk>
2022-09-09 10:27:59 +0100

    [PS-1387] Desktop: Remove "delete account" from settings (#3406)

--- apps/desktop/src/app/accounts/settings.component.ts
+++ apps/desktop/src/app/accounts/settings.component.ts
@@ -18,8 +18,6 @@ import { isWindowsStore } from "@bitwarden/electron/utils";
 
 import { SetPinComponent } from "../components/set-pin.component";
 
-import { DeleteAccountComponent } from "./delete-account.component";
-
 @Component({
   selector: "app-settings",
   templateUrl: "settings.component.html",
@@ -438,9 +436,5 @@ export class SettingsComponent implements OnInit {
     await this.stateService.setEnableBrowserIntegrationFingerprint(
       this.enableBrowserIntegrationFingerprint
     );
-  }
-
-  async openDeleteAccount() {
-    this.modalService.open(DeleteAccountComponent, { replaceTopModal: true });
   }
 }
