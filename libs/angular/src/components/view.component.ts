acf332b689625e1f9179d4f9113e6f0c8d775d27
André Filipe da Silva Bispo <abispo@bitwarden.com>
2022-09-13 17:56:10 +0100

    [SG-659] Classic 2019 plans no longer generating TOTP codes for Free users (#3509)
    * [SG-659] Fixed TOTP code visibility for users with classic plans.
    
    * [SG-659] Refactored condition.
    
    * [SG-659] Ran prettier
    
    * [SG-659] Simplified expression.

--- libs/angular/src/components/view.component.ts
+++ libs/angular/src/components/view.component.ts
@@ -50,6 +50,7 @@ export class ViewComponent implements OnDestroy, OnInit {
   showCardNumber: boolean;
   showCardCode: boolean;
   canAccessPremium: boolean;
+  showPremiumRequiredTotp: boolean;
   totpCode: string;
   totpCodeFormatted: string;
   totpDash: number;
@@ -108,6 +109,8 @@ export class ViewComponent implements OnDestroy, OnInit {
     const cipher = await this.cipherService.get(this.cipherId);
     this.cipher = await cipher.decrypt();
     this.canAccessPremium = await this.stateService.getCanAccessPremium();
+    this.showPremiumRequiredTotp =
+      this.cipher.login.totp && !this.canAccessPremium && !this.cipher.organizationUseTotp;
 
     if (
       this.cipher.type === CipherType.Login &&
