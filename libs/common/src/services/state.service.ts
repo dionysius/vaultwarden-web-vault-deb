750ff736cd80973056824e03acc876934b9af76e
Matt Gibson <mgibson@bitwarden.com>
2022-12-06 16:26:42 -0600

    [ps-2003] Ps/ps 1854 fix pin (#4193)
    * Await in `has` calls.
    
    * Add disk cache to browser synced items
    
    Note: `Map` doesn't serialize nicely so it's easier to swap over to a
    `Record` object for out cache
    
    * Mock and await init promises in tests
    
    * Remove redundant settings checks

--- libs/common/src/services/state.service.ts
+++ libs/common/src/services/state.service.ts
@@ -79,7 +79,7 @@ export class StateService<
   private hasBeenInited = false;
   private isRecoveredSession = false;
 
-  private accountDiskCache = new Map<string, TAccount>();
+  protected accountDiskCache = new BehaviorSubject<Record<string, TAccount>>({});
 
   // default account serializer, must be overridden by child class
   protected accountDeserializer = Account.fromJSON as (json: Jsonify<TAccount>) => TAccount;
@@ -2383,7 +2383,7 @@ export class StateService<
     }
 
     if (this.useAccountCache) {
-      const cachedAccount = this.accountDiskCache.get(options.userId);
+      const cachedAccount = this.accountDiskCache.value[options.userId];
       if (cachedAccount != null) {
         return cachedAccount;
       }
@@ -2397,9 +2397,7 @@ export class StateService<
         ))
       : await this.storageService.get<TAccount>(options.userId, options);
 
-    if (this.useAccountCache) {
-      this.accountDiskCache.set(options.userId, account);
-    }
+    this.setDiskCache(options.userId, account);
     return account;
   }
 
@@ -2430,9 +2428,7 @@ export class StateService<
 
     await storageLocation.save(`${options.userId}`, account, options);
 
-    if (this.useAccountCache) {
-      this.accountDiskCache.delete(options.userId);
-    }
+    this.deleteDiskCache(options.userId);
   }
 
   protected async saveAccountToMemory(account: TAccount): Promise<void> {
@@ -2643,9 +2639,7 @@ export class StateService<
       userId = userId ?? state.activeUserId;
       delete state.accounts[userId];
 
-      if (this.useAccountCache) {
-        this.accountDiskCache.delete(userId);
-      }
+      this.deleteDiskCache(userId);
 
       return state;
     });
@@ -2769,6 +2763,20 @@ export class StateService<
 
       await this.setState(updatedState);
     });
+  }
+
+  private setDiskCache(key: string, value: TAccount, options?: StorageOptions) {
+    if (this.useAccountCache) {
+      this.accountDiskCache.value[key] = value;
+      this.accountDiskCache.next(this.accountDiskCache.value);
+    }
+  }
+
+  private deleteDiskCache(key: string) {
+    if (this.useAccountCache) {
+      delete this.accountDiskCache.value[key];
+      this.accountDiskCache.next(this.accountDiskCache.value);
+    }
   }
 }
 
