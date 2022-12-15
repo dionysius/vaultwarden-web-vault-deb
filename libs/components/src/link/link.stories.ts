002c3f6368a067cf3ab03f8297112f625c5b5bfa
Patrick H. Lauke <redux@splintered.co.uk>
2022-12-15 14:13:09 +0000

    [PS-1793] Desktop/Browser/Web: tweak disclosure widget design (#3951)
    * Move chevron/arrow to start of disclosure widget
    
    in addition, changes the only expand/collapse disclosure widget like this on the web client to use the same `<h3><button>...</button></h3>` structure as on browser extension and desktop app
    
    * Change collapsed/expanded icons
    
    Make them more understandable and consistent with other expand/collapse controls
    
    * Harmonise desktop +/- controls to use arrow/chevron icons as well
    
    also removes the incorrect `A11yTitle` in the generator that currently overrides the visible "Options" text (leading to a failure of WCAG 2.5.3 Label in Name)
    
    * Change the icons for the expand/collapse disclosure widget in SSO component
    
    * Expand icon explanation
    
    plus minor typo cleanup
    
    * Add patch for Send button focus outline

--- libs/components/src/link/link.stories.ts
+++ libs/components/src/link/link.stories.ts
@@ -39,8 +39,8 @@ const ButtonTemplate: Story<ButtonLinkDirective> = (args: ButtonLinkDirective) =
     </div>
     <div class="tw-block tw-p-2">
       <button bitLink [linkType]="linkType">
+        <i class="bwi bwi-fw bwi-sm bwi-angle-right" aria-hidden="true"></i>
         Chevron Icon Button
-        <i class="bwi bwi-fw bwi-sm bwi-angle-down" aria-hidden="true"></i>
       </button>
     </div>
     <div class="tw-block tw-p-2">
@@ -65,8 +65,8 @@ const AnchorTemplate: Story<AnchorLinkDirective> = (args: AnchorLinkDirective) =
     </div>
     <div class="tw-block tw-p-2">
       <a bitLink [linkType]="linkType" href="#">
+        <i class="bwi bwi-fw bwi-sm bwi-angle-right" aria-hidden="true"></i>
         Chevron Icon Anchor
-        <i class="bwi bwi-fw bwi-sm bwi-angle-down" aria-hidden="true"></i>
       </a>
     </div>
     <div class="tw-block tw-p-2">
