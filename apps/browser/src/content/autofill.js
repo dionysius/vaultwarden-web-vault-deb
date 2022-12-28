c10e93c0d9cf306a592fbf6d8b1619e9a14cdc05
Matt Gibson <mgibson@bitwarden.com>
2022-12-28 09:49:38 -0500

    Handle null value URLs in autofiller script (#4277)

--- apps/browser/src/content/autofill.js
+++ apps/browser/src/content/autofill.js
@@ -43,6 +43,7 @@
   10. Handle sandbox iframe and sandbox rule in CSP
   11. Work on array of saved urls instead of just one to determine if we should autofill non-https sites
   12. Remove setting of attribute com.browser.browser.userEdited on user-inputs
+  13. Handle null value URLs in urlNotSecure
   */
 
   function collect(document, undefined) {
@@ -632,7 +633,7 @@
               return false;
           }
 
-          return savedURLs.some(url => url.indexOf('https://') === 0) && 'http:' === document.location.protocol && (passwordInputs = document.querySelectorAll('input[type=password]'),
+          return savedURLs.some(url => url?.indexOf('https://') === 0) && 'http:' === document.location.protocol && (passwordInputs = document.querySelectorAll('input[type=password]'),
               0 < passwordInputs.length && (confirmResult = confirm('Warning: This is an unsecured HTTP page, and any information you submit can potentially be seen and changed by others. This Login was originally saved on a secure (HTTPS) page.\n\nDo you still wish to fill this login?'),
                   0 == confirmResult)) ? true : false;
       }
