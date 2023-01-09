6b1a72851a37cb4e66c5d6194b617779b567572b
cyprain-okeke <108260115+cyprain-okeke@users.noreply.github.com>
2023-01-09 12:59:08 +0100

    Fixing Successful toast message does not appear (#4421)

--- libs/angular/src/components/send/add-edit.component.ts
+++ libs/angular/src/components/send/add-edit.component.ts
@@ -201,25 +201,14 @@ export class AddEditComponent implements OnInit, OnDestroy {
       }
       this.onSavedSend.emit(this.send);
       if (this.copyLink && this.link != null) {
-        const copySuccess = await this.copyLinkToClipboard(this.link);
-        if (copySuccess ?? true) {
-          this.platformUtilsService.showToast(
-            "success",
-            null,
-            this.i18nService.t(this.editMode ? "editedSend" : "createdSend")
-          );
-        } else {
-          await this.platformUtilsService.showDialog(
-            this.i18nService.t(this.editMode ? "editedSend" : "createdSend"),
-            null,
-            this.i18nService.t("ok"),
-            null,
-            "success",
-            null
-          );
-          await this.copyLinkToClipboard(this.link);
-        }
+        await this.handleCopyLinkToClipboard();
+        return;
       }
+      this.platformUtilsService.showToast(
+        "success",
+        null,
+        this.i18nService.t(this.editMode ? "editedSend" : "createdSend")
+      );
     });
     try {
       await this.formPromise;
@@ -307,5 +296,25 @@ export class AddEditComponent implements OnInit, OnDestroy {
   protected togglePasswordVisible() {
     this.showPassword = !this.showPassword;
     document.getElementById("password").focus();
+  }
+  private async handleCopyLinkToClipboard() {
+    const copySuccess = await this.copyLinkToClipboard(this.link);
+    if (copySuccess ?? true) {
+      this.platformUtilsService.showToast(
+        "success",
+        null,
+        this.i18nService.t(this.editMode ? "editedSend" : "createdSend")
+      );
+    } else {
+      await this.platformUtilsService.showDialog(
+        this.i18nService.t(this.editMode ? "editedSend" : "createdSend"),
+        null,
+        this.i18nService.t("ok"),
+        null,
+        "success",
+        null
+      );
+      await this.copyLinkToClipboard(this.link);
+    }
   }
 }
