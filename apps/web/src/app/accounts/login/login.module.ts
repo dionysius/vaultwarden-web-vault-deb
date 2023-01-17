a9db6b2d7d48d65c0bffe874424b49c7f01647db
jcuenca95 <42175335+jcuenca95@users.noreply.github.com>
2023-01-17 16:13:28 +0100

    [PS-1872] remember me label triggers toggle (#4046)
    * remember me label triggers toggle
    
    * fix using new bitCheckbox
    
    * changes using merge
    
    * Update apps/web/src/app/accounts/login/login.component.html
    
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>
    
    * CheckboxModule imported
    
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>

--- apps/web/src/app/accounts/login/login.module.ts
+++ apps/web/src/app/accounts/login/login.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 
+import { CheckboxModule } from "@bitwarden/components";
+
 import { SharedModule } from "../../shared";
 
 import { LoginWithDeviceComponent } from "./login-with-device.component";
 import { LoginComponent } from "./login.component";
 
 @NgModule({
-  imports: [SharedModule],
+  imports: [SharedModule, CheckboxModule],
   declarations: [LoginComponent, LoginWithDeviceComponent],
   exports: [LoginComponent, LoginWithDeviceComponent],
 })
