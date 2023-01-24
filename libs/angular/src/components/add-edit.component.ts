237663260363dfaead9cf677347009cfdebfcae4
Andreas Coroiu <acoroiu@bitwarden.com>
2023-01-24 14:29:29 +0100

    [EC-822] fix: filter provider orgs (#4489)

--- libs/angular/src/components/add-edit.component.ts
+++ libs/angular/src/components/add-edit.component.ts
@@ -9,7 +9,10 @@ import { FolderService } from "@bitwarden/common/abstractions/folder/folder.serv
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/abstractions/log.service";
 import { MessagingService } from "@bitwarden/common/abstractions/messaging.service";
-import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
+import {
+  isNotProviderUser,
+  OrganizationService,
+} from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
 import { PasswordRepromptService } from "@bitwarden/common/abstractions/passwordReprompt.service";
 import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
 import { PolicyService } from "@bitwarden/common/abstractions/policy/policy.service.abstraction";
@@ -186,11 +189,14 @@ export class AddEditComponent implements OnInit, OnDestroy {
     }
 
     const orgs = await this.organizationService.getAll();
-    orgs.sort(Utils.getSortFunction(this.i18nService, "name")).forEach((o) => {
-      if (o.enabled && o.status === OrganizationUserStatusType.Confirmed) {
-        this.ownershipOptions.push({ name: o.name, value: o.id });
-      }
-    });
+    orgs
+      .filter(isNotProviderUser)
+      .sort(Utils.getSortFunction(this.i18nService, "name"))
+      .forEach((o) => {
+        if (o.enabled && o.status === OrganizationUserStatusType.Confirmed) {
+          this.ownershipOptions.push({ name: o.name, value: o.id });
+        }
+      });
     if (!this.allowPersonal) {
       this.organizationId = this.ownershipOptions[0].value;
     }
