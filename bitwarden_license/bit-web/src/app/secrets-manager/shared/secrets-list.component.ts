2b95c786d82c8c8c51672be8ae3eff325e5a6cba
Will Martin <contact@willmartian.com>
2023-03-03 09:00:20 -0500

    [SM-595] add project sort to secrets list (#4914)

--- bitwarden_license/bit-web/src/app/secrets-manager/shared/secrets-list.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/shared/secrets-list.component.ts
@@ -77,4 +77,14 @@ export class SecretsListComponent implements OnDestroy {
       this.restoreSecretsEvent.emit(this.selection.selected);
     }
   }
+
+  sortProjects = (a: SecretListView, b: SecretListView): number => {
+    const aProjects = a.projects;
+    const bProjects = b.projects;
+    if (aProjects.length !== bProjects.length) {
+      return aProjects.length - bProjects.length;
+    }
+
+    return aProjects[0]?.name.localeCompare(bProjects[0].name);
+  };
 }
