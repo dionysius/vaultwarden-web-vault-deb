5bc4fa62efbc5b5fe93e00c2ca0fd410b8c19e7e
Oscar Hinton <Hinton@users.noreply.github.com>
2023-03-21 11:31:11 +0100

    [PM-1451] Remove unused export.module.scss (#5012)

--- apps/web/src/scss/export.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ThemeVariableExport {
-  lightInputColor: string;
-  lightInputPlaceholderColor: string;
-  darkInputColor: string;
-  darkInputPlaceholderColor: string;
-}
-
-export const ThemeVariables: ThemeVariableExport;
-export default ThemeVariables;
