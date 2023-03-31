68e973a865888c49696905bad2059edacfcfcdc5
Vincent Salucci <vincesalucci21@gmail.com>
2023-03-31 10:42:49 -0500

    fix: update import paths for index move, refs AC-1202

--- apps/web/src/app/admin-console/organizations/members/components/member-dialog/member-dialog.module.ts
+++ apps/web/src/app/admin-console/organizations/members/components/member-dialog/member-dialog.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 
 import { RadioButtonModule } from "@bitwarden/components";
 
-import { SharedOrganizationModule } from "../../../../../organizations/shared";
+import { SharedOrganizationModule } from "../../../shared";
 
 import { MemberDialogComponent } from "./member-dialog.component";
 import { NestedCheckboxComponent } from "./nested-checkbox.component";
