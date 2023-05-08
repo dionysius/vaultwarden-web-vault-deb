d4bfad955e6f98be174f1d427950684dbbf292ef
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-05-08 13:22:06 -0400

    [PM-2089] Fix Collections not nesting properly in the Org Vault (#5369)
    (cherry picked from commit 9bbaae6ef2da09f04aef5fd75f7b0fbb1f4a6d52)

---
+++ apps/web/src/app/vault/utils/collection-utils.spec.ts
@@ -0,0 +1,28 @@
+import { CollectionView } from "@bitwarden/common/admin-console/models/view/collection.view";
+
+import { getNestedCollectionTree } from "./collection-utils";
+
+describe("CollectionUtils Service", () => {
+  describe("getNestedCollectionTree", () => {
+    it("should return collections properly sorted if provided out of order", () => {
+      // Arrange
+      const collections: CollectionView[] = [];
+
+      const parentCollection = new CollectionView();
+      parentCollection.name = "Parent";
+
+      const childCollection = new CollectionView();
+      childCollection.name = "Parent/Child";
+
+      collections.push(childCollection);
+      collections.push(parentCollection);
+
+      // Act
+      const result = getNestedCollectionTree(collections);
+
+      // Assert
+      expect(result[0].node.name).toBe("Parent");
+      expect(result[0].children[0].node.name).toBe("Child");
+    });
+  });
+});
