cd85a4c03903c9e9aa92106d77a847e988a4fd2e
Bernd Schoolmann <mail@quexten.com>
2023-06-12 20:59:59 +0200

    Automatically load SIMD binary for argon2 (#4921)

--- apps/desktop/webpack.main.js
+++ apps/desktop/webpack.main.js
@@ -74,6 +74,7 @@ const main = {
         { from: "./src/images", to: "images" },
         { from: "./src/locales", to: "locales" },
         "../../node_modules/argon2-browser/dist/argon2.wasm",
+        "../../node_modules/argon2-browser/dist/argon2-simd.wasm",
       ],
     }),
     new EnvironmentPlugin({
