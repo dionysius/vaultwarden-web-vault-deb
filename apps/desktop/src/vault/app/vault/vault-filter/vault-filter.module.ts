30bb88006224d0821a64b9ac376c6fab6aa1549d
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2023-06-19 15:42:15 +1000

    Move desktop vault filters to vault team (#5574)

--- apps/desktop/src/vault/app/vault/vault-filter/vault-filter.module.ts
+++ apps/desktop/src/vault/app/vault/vault-filter/vault-filter.module.ts
@@ -5,10 +5,9 @@ import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { DeprecatedVaultFilterService as DeprecatedVaultFilterServiceAbstraction } from "@bitwarden/angular/vault/abstractions/deprecated-vault-filter.service";
 import { VaultFilterService } from "@bitwarden/angular/vault/vault-filter/services/vault-filter.service";
 
-import { CollectionFilterComponent } from "../../../../admin-console/vault-filters/collection-filter.component";
-import { OrganizationFilterComponent } from "../../../../admin-console/vault-filters/organization-filter.component";
-
+import { CollectionFilterComponent } from "./filters/collection-filter.component";
 import { FolderFilterComponent } from "./filters/folder-filter.component";
+import { OrganizationFilterComponent } from "./filters/organization-filter.component";
 import { StatusFilterComponent } from "./filters/status-filter.component";
 import { TypeFilterComponent } from "./filters/type-filter.component";
 import { VaultFilterComponent } from "./vault-filter.component";
