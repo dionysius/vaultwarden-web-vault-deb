70f115c8f5929defb77b800393a8e7404d9a296a
Michał Chęciński <mchecinski@bitwarden.com>
2023-08-17 10:33:07 +0200

    [DEVOPS-1438] Migrate our mac os desktop notary tool (#5741)
    * Change altool to notarytool for desktop app notarizing
    
    * Comment for testing
    
    * Add team id
    
    * Try to notarize with old method
    
    * TEst vaslues
    
    * Change after-sign notarization option
    
    * CHange notarization in package
    
    * Fix
    
    * fix
    
    * Maybe fix
    
    * Use altool to upload
    
    * Re enable if after testing

--- apps/desktop/scripts/after-sign.js
+++ apps/desktop/scripts/after-sign.js
@@ -53,8 +53,9 @@ async function run(context) {
     const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
     const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
     return await notarize({
-      appBundleId: "com.bitwarden.desktop",
+      tool: "notarytool",
       appPath: appPath,
+      teamId: "LTZ2PFU5D6",
       appleId: appleId,
       appleIdPassword: appleIdPassword,
     });
