1d7360bfdd36248f19aa048bd1012353e2c6f3b9
Dave Nicolson <david.nicolson@gmail.com>
2023-09-01 12:06:49 +0200

    [PS-1438] Prevent new line feed when selecting and copying passwords (#3460)
    * Prevent new line feed when selecting password
    
    * Prevent new line feed when copying password

--- libs/angular/src/directives/copy-text.directive.ts
+++ libs/angular/src/directives/copy-text.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
+import { ClientType } from "@bitwarden/common/enums";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 
 @Directive({
@@ -15,6 +16,9 @@ export class CopyTextDirective {
       return;
     }
 
-    this.platformUtilsService.copyToClipboard(this.copyText, { window: window });
+    const timeout = this.platformUtilsService.getClientType() === ClientType.Desktop ? 100 : 0;
+    setTimeout(() => {
+      this.platformUtilsService.copyToClipboard(this.copyText, { window: window });
+    }, timeout);
   }
 }
