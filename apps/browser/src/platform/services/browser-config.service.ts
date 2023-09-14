2be4f43eb6edb2b2ac3b3cb9c1f016aa74e2cd55
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2023-09-14 20:29:41 +1000

    [PM-3883] Fix ConfigService.serverConfig$ initial values and error handling (#6272)
    * Always fetch ServerConfig from server, use stored value as fallback
    
    * Handle errors in server fetch
    
    (cherry picked from commit 931a2258e29fda19b281e417b997dbc31fe3be32)

--- apps/browser/src/platform/services/browser-config.service.ts
+++ apps/browser/src/platform/services/browser-config.service.ts
@@ -4,6 +4,7 @@ import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
 import { ConfigApiServiceAbstraction } from "@bitwarden/common/platform/abstractions/config/config-api.service.abstraction";
 import { ServerConfig } from "@bitwarden/common/platform/abstractions/config/server-config";
 import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
+import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
 import { ConfigService } from "@bitwarden/common/platform/services/config/config.service";
 
@@ -19,8 +20,9 @@ export class BrowserConfigService extends ConfigService {
     configApiService: ConfigApiServiceAbstraction,
     authService: AuthService,
     environmentService: EnvironmentService,
+    logService: LogService,
     subscribe = false
   ) {
-    super(stateService, configApiService, authService, environmentService, subscribe);
+    super(stateService, configApiService, authService, environmentService, logService, subscribe);
   }
 }
