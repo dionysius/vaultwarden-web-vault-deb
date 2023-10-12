84bafe5e73ac0fab6a2c4f3cb9942bca45760009
Will Martin <contact@willmartian.com>
2023-10-12 10:34:53 -0400

    [CL-104] fix overlay + virtual scroll view recycling bug (#6179)
    * close menu overlay when no longer visible
    
    * prevent infinite loop in fallback-src directive
    
    * block scrolling when menu is open
    
    * disable view recycling; use reposition strategy

--- libs/angular/src/directives/fallback-src.directive.ts
+++ libs/angular/src/directives/fallback-src.directive.ts
@@ -6,9 +6,15 @@ import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 export class FallbackSrcDirective {
   @Input("appFallbackSrc") appFallbackSrc: string;
 
+  /** Only try setting the fallback once. This prevents an infinite loop if the fallback itself is missing. */
+  private tryFallback = true;
+
   constructor(private el: ElementRef) {}
 
   @HostListener("error") onError() {
-    this.el.nativeElement.src = this.appFallbackSrc;
+    if (this.tryFallback) {
+      this.el.nativeElement.src = this.appFallbackSrc;
+      this.tryFallback = false;
+    }
   }
 }
