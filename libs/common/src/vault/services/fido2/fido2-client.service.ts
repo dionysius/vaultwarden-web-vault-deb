93b912bce40db570fb3d1156d52477f8a04fc787
Todd Martin <tmartin@bitwarden.com>
2023-10-25 12:54:22 -0400

    Removed preferred from verification status that prompts
    (cherry picked from commit f988c3c4f26564e46af7f7137b941ddfe58d8df7)

--- libs/common/src/vault/services/fido2/fido2-client.service.ts
+++ libs/common/src/vault/services/fido2/fido2-client.service.ts
@@ -365,7 +365,6 @@ function mapToMakeCredentialParams({
 
   const requireUserVerification =
     params.authenticatorSelection?.userVerification === "required" ||
-    params.authenticatorSelection?.userVerification === "preferred" ||
     params.authenticatorSelection?.userVerification === undefined;
 
   return {
@@ -404,9 +403,7 @@ function mapToGetAssertionParams({
     }));
 
   const requireUserVerification =
-    params.userVerification === "required" ||
-    params.userVerification === "preferred" ||
-    params.userVerification === undefined;
+    params.userVerification === "required" || params.userVerification === undefined;
 
   return {
     rpId: params.rpId,
