d52e4dc56ea405fc3d01faa1c113912771ef4253
ttalty <144813356+ttalty@users.noreply.github.com>
2023-10-31 11:06:04 -0400

    Adding the item-share feature flag (#6741)

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -6,6 +6,7 @@ export enum FeatureFlag {
   PasswordlessLogin = "passwordless-login",
   AutofillV2 = "autofill-v2",
   BrowserFilelessImport = "browser-fileless-import",
+  ItemShare = "item-share",
 }
 
 // Replace this with a type safe lookup of the feature flag values in PM-2282
