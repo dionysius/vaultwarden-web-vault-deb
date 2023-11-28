724c7b2e7977e8af5008f99d4d38ba02593f67f2
Ike <137194738+ike-kottlowski@users.noreply.github.com>
2023-11-28 09:45:13 -0800

    updated bit_lic/app-routing module (#6995)
    (cherry picked from commit a342ce5d9a535e06cc801fee6fc8ad72f778530f)

--- bitwarden_license/bit-web/src/app/app-routing.module.ts
+++ bitwarden_license/bit-web/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { deepLinkGuard } from "@bitwarden/web-vault/app/auth/guards/deep-link.guard";
+
 import { ProvidersModule } from "./admin-console/providers/providers.module";
 
 const routes: Routes = [
   {
     path: "providers",
+    canActivate: [deepLinkGuard()],
     loadChildren: () => ProvidersModule,
   },
   {
