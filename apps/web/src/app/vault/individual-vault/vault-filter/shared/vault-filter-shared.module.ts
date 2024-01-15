36b2d8b6b2f3d960f8f42c12edb002d14f5f7f56
Oscar Hinton <Hinton@users.noreply.github.com>
2024-01-15 12:10:31 +0100

    [PM-5345] Update vault-search to use bit-search (#7273)
    Migrate vault search to use the bit-search component.

--- apps/web/src/app/vault/individual-vault/vault-filter/shared/vault-filter-shared.module.ts
+++ apps/web/src/app/vault/individual-vault/vault-filter/shared/vault-filter-shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 
+import { SearchModule } from "@bitwarden/components";
+
 import { SharedModule } from "../../../../shared";
 
 import { VaultFilterSectionComponent } from "./components/vault-filter-section.component";
 
 @NgModule({
-  imports: [SharedModule],
+  imports: [SharedModule, SearchModule],
   declarations: [VaultFilterSectionComponent],
-  exports: [SharedModule, VaultFilterSectionComponent],
+  exports: [SharedModule, VaultFilterSectionComponent, SearchModule],
 })
 export class VaultFilterSharedModule {}
