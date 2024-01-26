53be4946de4c55c338c950c7043ead120ab0ceeb
Daniel James Smith <2670567+djsmith85@users.noreply.github.com>
2024-01-26 17:15:28 +0100

    [PM-5717] Fix calling methods on undefined in biometrics service (#7559)
    * Fix calling init() on undefined in biometrics.service.ts
    
    * Add guard on osSupportsBiometric
    
    * Create NoopBiometricsService instead of method guards
    
    ---------
    
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>

---
+++ apps/desktop/src/platform/main/biometric/biometric.noop.main.ts
@@ -0,0 +1,34 @@
+import { OsBiometricService } from "./biometrics.service.abstraction";
+
+export default class NoopBiometricsService implements OsBiometricService {
+  constructor() {}
+
+  async init() {}
+
+  async osSupportsBiometric(): Promise<boolean> {
+    return false;
+  }
+
+  async getBiometricKey(
+    service: string,
+    storageKey: string,
+    clientKeyHalfB64: string,
+  ): Promise<string | null> {
+    return null;
+  }
+
+  async setBiometricKey(
+    service: string,
+    storageKey: string,
+    value: string,
+    clientKeyPartB64: string | undefined,
+  ): Promise<void> {
+    return;
+  }
+
+  async deleteBiometricKey(service: string, key: string): Promise<void> {}
+
+  async authenticateBiometric(): Promise<boolean> {
+    throw new Error("Not supported on this platform");
+  }
+}
--- apps/desktop/src/platform/main/biometric/biometrics.service.ts
+++ apps/desktop/src/platform/main/biometric/biometrics.service.ts
@@ -27,6 +27,8 @@ export class BiometricsService implements BiometricsServiceAbstraction {
       this.loadWindowsHelloService();
     } else if (platform === "darwin") {
       this.loadMacOSService();
+    } else {
+      this.loadNoopBiometricsService();
     }
   }
 
@@ -45,6 +47,12 @@ export class BiometricsService implements BiometricsServiceAbstraction {
     // eslint-disable-next-line
     const BiometricDarwinMain = require("./biometric.darwin.main").default;
     this.platformSpecificService = new BiometricDarwinMain(this.i18nService, this.stateService);
+  }
+
+  private loadNoopBiometricsService() {
+    // eslint-disable-next-line
+    const NoopBiometricsService = require("./biometric.noop.main").default;
+    this.platformSpecificService = new NoopBiometricsService();
   }
 
   async init() {
