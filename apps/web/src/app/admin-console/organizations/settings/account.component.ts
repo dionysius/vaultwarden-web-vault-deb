c2e24f8ca494bd2ff4c2d2e348a43bd5cd41576d
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-02-05 22:54:07 +0100

    allow renaming of organizations

--- apps/web/src/app/admin-console/organizations/settings/account.component.ts
+++ apps/web/src/app/admin-console/organizations/settings/account.component.ts
@@ -94,7 +94,7 @@ export class AccountComponent implements OnInit, OnDestroy {
   ) {}
 
   async ngOnInit() {
-    this.selfHosted = this.platformUtilsService.isSelfHost();
+    this.selfHosted = false; // set to false so we can rename organizations
 
     const userId = await firstValueFrom(getUserId(this.accountService.activeAccount$));
     this.route.params
