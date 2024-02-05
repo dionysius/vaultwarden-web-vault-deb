235edbf03befba6ba0fcf1c55fe8b7d1f262fad8
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-02-06 00:55:31 +0100

    disable billing tab

--- apps/web/src/app/admin-console/organizations/layouts/organization-layout.component.ts
+++ apps/web/src/app/admin-console/organizations/layouts/organization-layout.component.ts
@@ -186,6 +186,7 @@ export class OrganizationLayoutComponent implements OnInit {
   }
 
   canShowBillingTab(organization: Organization): boolean {
+    return false; // disable billing tab in Vaultwarden
     return canAccessBillingTab(organization);
   }
 
