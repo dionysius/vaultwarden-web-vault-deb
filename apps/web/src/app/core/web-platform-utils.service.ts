6db3e7c735d9104c99031656e2d7a8eb3f26c354
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-02-05 22:37:56 +0100

    treat Vaultwarden as self hosted

--- apps/web/src/app/core/web-platform-utils.service.ts
+++ apps/web/src/app/core/web-platform-utils.service.ts
@@ -135,14 +135,17 @@ export class WebPlatformUtilsService implements PlatformUtilsService {
   }
 
   isDev(): boolean {
+    return false; // treat Vaultwarden as production ready
     return process.env.NODE_ENV === "development";
   }
 
   isSelfHost(): boolean {
+    return true; // treat Vaultwarden as self hosted
     return WebPlatformUtilsService.isSelfHost();
   }
 
   static isSelfHost(): boolean {
+    return true; // treat Vaultwarden as self hosted
     return process.env.ENV.toString() === "selfhosted";
   }
 
