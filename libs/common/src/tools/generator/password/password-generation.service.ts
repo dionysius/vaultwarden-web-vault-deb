64381cbae0495658fd134452ecab080b5c6eb1b2
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-02-09 11:33:52 -0500

    check credential type explicitly when loading evaluator (#7791)

--- libs/common/src/tools/generator/password/password-generation.service.ts
+++ libs/common/src/tools/generator/password/password-generation.service.ts
@@ -195,9 +195,10 @@ export class PasswordGenerationService implements PasswordGenerationServiceAbstr
       options.type = policy.defaultType;
     }
 
-    const evaluator = options.type
-      ? new PasswordGeneratorOptionsEvaluator(policy)
-      : new PassphraseGeneratorOptionsEvaluator(policy);
+    const evaluator =
+      options.type == "password"
+        ? new PasswordGeneratorOptionsEvaluator(policy)
+        : new PassphraseGeneratorOptionsEvaluator(policy);
 
     // Ensure the options to pass the current rules
     const withPolicy = evaluator.applyPolicy(options);
@@ -344,9 +345,10 @@ export class PasswordGenerationService implements PasswordGenerationServiceAbstr
     options: PasswordGeneratorOptions,
     enforcedPolicyOptions: PasswordGeneratorPolicyOptions,
   ) {
-    const evaluator = options.type
-      ? new PasswordGeneratorOptionsEvaluator(enforcedPolicyOptions)
-      : new PassphraseGeneratorOptionsEvaluator(enforcedPolicyOptions);
+    const evaluator =
+      options.type == "password"
+        ? new PasswordGeneratorOptionsEvaluator(enforcedPolicyOptions)
+        : new PassphraseGeneratorOptionsEvaluator(enforcedPolicyOptions);
 
     const evaluatedOptions = evaluator.applyPolicy(options);
     const santizedOptions = evaluator.sanitize(evaluatedOptions);
