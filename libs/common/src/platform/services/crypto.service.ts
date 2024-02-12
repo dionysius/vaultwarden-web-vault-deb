f786c8ce93facd805a85cbc68891eb83fb61b60a
Jake Fink <jfink@bitwarden.com>
2024-02-12 11:12:35 -0500

    clear ever had user key for logged out user instead of active (#7911)

--- libs/common/src/platform/services/crypto.service.ts
+++ libs/common/src/platform/services/crypto.service.ts
@@ -625,7 +625,7 @@ export class CryptoService implements CryptoServiceAbstraction {
     await this.clearProviderKeys(false, userId);
     await this.clearKeyPair(false, userId);
     await this.clearPinKeys(userId);
-    await this.activeUserEverHadUserKey.update(() => null);
+    await this.stateProvider.setUserState(USER_EVER_HAD_USER_KEY, null, userId);
   }
 
   async rsaEncrypt(data: Uint8Array, publicKey?: Uint8Array): Promise<EncString> {
