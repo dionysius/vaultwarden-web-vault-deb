fda656afaa5dcd5084013928ce9f121957aa0f36
Jake Fink <jfink@bitwarden.com>
2024-02-22 12:41:43 -0500

    add properties to class itself so we don't need constructor (#8051)

--- apps/web/src/app/vault/individual-vault/vault-filter/components/link-sso.directive.ts
+++ apps/web/src/app/vault/individual-vault/vault-filter/components/link-sso.directive.ts
@@ -1,19 +1,7 @@
 import { AfterContentInit, Directive, HostListener, Input } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
 
 import { SsoComponent } from "@bitwarden/angular/auth/components/sso.component";
-import { LoginStrategyServiceAbstraction } from "@bitwarden/auth/common";
-import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
-import { SsoLoginServiceAbstraction } from "@bitwarden/common/auth/abstractions/sso-login.service.abstraction";
-import { ConfigServiceAbstraction } from "@bitwarden/common/platform/abstractions/config/config.service.abstraction";
-import { CryptoFunctionService } from "@bitwarden/common/platform/abstractions/crypto-function.service";
-import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
-import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
-import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
-import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
-import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
-import { PasswordGenerationServiceAbstraction } from "@bitwarden/common/tools/generator/password";
 
 @Directive({
   selector: "[app-link-sso]",
@@ -21,47 +9,13 @@ import { PasswordGenerationServiceAbstraction } from "@bitwarden/common/tools/ge
 export class LinkSsoDirective extends SsoComponent implements AfterContentInit {
   @Input() organization: Organization;
   returnUri = "/settings/organizations";
+  redirectUri = window.location.origin + "/sso-connector.html";
+  clientId = "web";
 
   @HostListener("click", ["$event"])
   async onClick($event: MouseEvent) {
     $event.preventDefault();
     await this.submit(this.returnUri, true);
-  }
-
-  constructor(
-    ssoLoginService: SsoLoginServiceAbstraction,
-    platformUtilsService: PlatformUtilsService,
-    i18nService: I18nService,
-    apiService: ApiService,
-    loginStrategyService: LoginStrategyServiceAbstraction,
-    router: Router,
-    route: ActivatedRoute,
-    cryptoFunctionService: CryptoFunctionService,
-    passwordGenerationService: PasswordGenerationServiceAbstraction,
-    stateService: StateService,
-    environmentService: EnvironmentService,
-    logService: LogService,
-    configService: ConfigServiceAbstraction,
-  ) {
-    super(
-      ssoLoginService,
-      loginStrategyService,
-      router,
-      i18nService,
-      route,
-      stateService,
-      platformUtilsService,
-      apiService,
-      cryptoFunctionService,
-      environmentService,
-      passwordGenerationService,
-      logService,
-      configService,
-    );
-
-    this.returnUri = "/settings/organizations";
-    this.redirectUri = window.location.origin + "/sso-connector.html";
-    this.clientId = "web";
   }
 
   async ngAfterContentInit() {
