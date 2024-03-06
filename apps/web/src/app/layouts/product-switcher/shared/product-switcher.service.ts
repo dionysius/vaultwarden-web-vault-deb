cd5cca1ef80c7a5dfca3c79c061c282c2cd81faf
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-03-06 01:48:07 +0100

    disable ads for other products

--- apps/web/src/app/layouts/product-switcher/shared/product-switcher.service.ts
+++ apps/web/src/app/layouts/product-switcher/shared/product-switcher.service.ts
@@ -19,11 +19,7 @@ import {
 } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { PolicyService } from "@bitwarden/common/admin-console/abstractions/policy/policy.service.abstraction";
 import { ProviderService } from "@bitwarden/common/admin-console/abstractions/provider.service";
-import {
-  OrganizationUserType,
-  PolicyType,
-  ProviderType,
-} from "@bitwarden/common/admin-console/enums";
+import { PolicyType, ProviderType } from "@bitwarden/common/admin-console/enums";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { Provider } from "@bitwarden/common/admin-console/models/domain/provider";
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
@@ -217,11 +213,7 @@ export class ProductSwitcherService {
         // Check if SM ads should be disabled for any organization
         // SM ads are only disabled if the feature flag is enabled AND
         // the user is a regular User (not Admin or Owner) in an organization that has useDisableSMAdsForUsers enabled
-        const shouldDisableSMAds =
-          removeSecretsManagerAdsFlag &&
-          orgs.some(
-            (org) => org.useDisableSMAdsForUsers === true && org.type === OrganizationUserType.User,
-          );
+        const shouldDisableSMAds = true; // always hide ads for secret manager in Vaultwarden
 
         const products = {
           pm: {
@@ -291,7 +283,7 @@ export class ProductSwitcherService {
           bento.push(products.ac);
         } else {
           if (!userHasSingleOrgPolicy) {
-            other.push(products.orgs);
+            // other.push(products.orgs); no add for Organizations in Vaultwarden
           }
         }
 
