c14091904b8b5ed2512ea8d12d0c8eeb27f8de11
Matt Gibson <mgibson@bitwarden.com>
2024-03-06 11:48:28 -0600

    Propose team as a first pass organization to state definitions (#8225)

--- libs/common/src/platform/state/state-definitions.ts
+++ libs/common/src/platform/state/state-definitions.ts
@@ -17,63 +17,70 @@ import { StateDefinition } from "./state-definition";
  *
  */
 
-export const ACCOUNT_MEMORY = new StateDefinition("account", "memory");
-
-export const CRYPTO_DISK = new StateDefinition("crypto", "disk");
-export const CRYPTO_MEMORY = new StateDefinition("crypto", "memory");
-
-export const SSO_DISK = new StateDefinition("ssoLogin", "disk");
-
-export const ENVIRONMENT_DISK = new StateDefinition("environment", "disk");
-
-export const VAULT_ONBOARDING = new StateDefinition("vaultOnboarding", "disk", {
-  web: "disk-local",
-});
-
-export const GENERATOR_DISK = new StateDefinition("generator", "disk");
-export const GENERATOR_MEMORY = new StateDefinition("generator", "memory");
-
-export const BIOMETRIC_SETTINGS_DISK = new StateDefinition("biometricSettings", "disk");
-
 // Admin Console
+
 export const ORGANIZATIONS_DISK = new StateDefinition("organizations", "disk");
 export const POLICIES_DISK = new StateDefinition("policies", "disk");
 export const PROVIDERS_DISK = new StateDefinition("providers", "disk");
 
-export const BILLING_DISK = new StateDefinition("billing", "disk");
+// Auth
 
-export const FOLDER_DISK = new StateDefinition("folder", "disk", { web: "memory" });
+export const ACCOUNT_MEMORY = new StateDefinition("account", "memory");
+export const SSO_DISK = new StateDefinition("ssoLogin", "disk");
 
-export const VAULT_SETTINGS_DISK = new StateDefinition("vaultSettings", "disk", {
-  web: "disk-local",
-});
-
-export const COLLECTION_DATA = new StateDefinition("collection", "disk", {
-  web: "memory",
-});
+// Autofill
 
 export const BADGE_SETTINGS_DISK = new StateDefinition("badgeSettings", "disk");
+export const USER_NOTIFICATION_SETTINGS_DISK = new StateDefinition(
+  "userNotificationSettings",
+  "disk",
+);
+
+// Billing
 
 export const AUTOFILL_SETTINGS_DISK = new StateDefinition("autofillSettings", "disk");
 export const AUTOFILL_SETTINGS_DISK_LOCAL = new StateDefinition("autofillSettingsLocal", "disk", {
   web: "disk-local",
 });
+export const BILLING_DISK = new StateDefinition("billing", "disk");
 
-export const VAULT_FILTER_DISK = new StateDefinition("vaultFilter", "disk", {
-  web: "disk-local",
-});
-
-export const USER_NOTIFICATION_SETTINGS_DISK = new StateDefinition(
-  "userNotificationSettings",
-  "disk",
-);
-export const CLEAR_EVENT_DISK = new StateDefinition("clearEvent", "disk");
+// Components
 
 export const NEW_WEB_LAYOUT_BANNER_DISK = new StateDefinition("newWebLayoutBanner", "disk", {
   web: "disk-local",
 });
 
+// Platform
+
+export const BIOMETRIC_SETTINGS_DISK = new StateDefinition("biometricSettings", "disk");
+export const CLEAR_EVENT_DISK = new StateDefinition("clearEvent", "disk");
+export const CRYPTO_DISK = new StateDefinition("crypto", "disk");
+export const CRYPTO_MEMORY = new StateDefinition("crypto", "memory");
+export const ENVIRONMENT_DISK = new StateDefinition("environment", "disk");
+
 // Secrets Manager
+
 export const SM_ONBOARDING_DISK = new StateDefinition("smOnboarding", "disk", {
+  web: "disk-local",
+});
+
+// Tools
+
+export const GENERATOR_DISK = new StateDefinition("generator", "disk");
+export const GENERATOR_MEMORY = new StateDefinition("generator", "memory");
+
+// Vault
+
+export const COLLECTION_DATA = new StateDefinition("collection", "disk", {
+  web: "memory",
+});
+export const FOLDER_DISK = new StateDefinition("folder", "disk", { web: "memory" });
+export const VAULT_FILTER_DISK = new StateDefinition("vaultFilter", "disk", {
+  web: "disk-local",
+});
+export const VAULT_ONBOARDING = new StateDefinition("vaultOnboarding", "disk", {
+  web: "disk-local",
+});
+export const VAULT_SETTINGS_DISK = new StateDefinition("vaultSettings", "disk", {
   web: "disk-local",
 });
