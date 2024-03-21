7a42b4ebc6f494da0de4cd70d575ede4fddcc307
Daniel James Smith <2670567+djsmith85@users.noreply.github.com>
2024-03-21 16:58:48 +0100

    [PM-6967] Replace app-callouts with bit-callouts on desktop (#8415)
    * Enable usage of the bit-callout within desktop
    
    Adding this should be temporary, with the vision being: All components are standalone and they'd depend/import the CalloutModule themselves if they need it
    
    * Replace vaultTimeoutPolicy callout in settings
    
    * Replace OwnershipPolicy callout in add-edit cipher
    
    * Replace vaultExportDisabled policy callout in export
    
    * Replace generator policy callout in generator
    
    * Replace policy callouts in add/edit Sends
    
    ---------
    
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>

--- apps/desktop/src/app/app.module.ts
+++ apps/desktop/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NgModule } from "@angular/core";
 
 import { ColorPasswordCountPipe } from "@bitwarden/angular/pipes/color-password-count.pipe";
 import { ColorPasswordPipe } from "@bitwarden/angular/pipes/color-password.pipe";
-import { DialogModule } from "@bitwarden/components";
+import { DialogModule, CalloutModule } from "@bitwarden/components";
 import { ExportScopeCalloutComponent } from "@bitwarden/vault-export-ui";
 
 import { AccessibilityCookieComponent } from "../auth/accessibility-cookie.component";
@@ -60,6 +60,7 @@ import { SendComponent } from "./tools/send/send.component";
     VaultFilterModule,
     LoginModule,
     DialogModule,
+    CalloutModule,
     DeleteAccountComponent,
     UserVerificationComponent,
     ExportScopeCalloutComponent,
