905d17787373ca7e4c41bd57457b50837fc7cb1d
Cesar Gonzalez <cesar.a.gonzalezcs@gmail.com>
2024-03-22 13:45:33 -0500

    [PM-4791] Injected content scripts prevent proper XML file display and disrupt XML responses (#8214)
    * [PM-4791] Injected content scripts prevent proper XML file display and disrupt XML responses
    
    * [PM-4791] Adjsuting reference for Fido2 script injection to ensure it only triggers on https protocol types

--- apps/browser/src/vault/fido2/content/content-script.ts
+++ apps/browser/src/vault/fido2/content/content-script.ts
@@ -138,6 +138,7 @@ async function run() {
   });
 }
 
-// FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run();
+// Only run the script if the document is an HTML document
+if (document.contentType === "text/html") {
+  void run();
+}
