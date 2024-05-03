ac4e8caaf685e1524e90df7a8998b34878141e2e
Stefan Melmuk <stefan.melmuk@gmail.com>
2024-05-03 18:37:39 +0200

    disable billing metadata

--- apps/web/src/app/admin-console/organizations/members/members.component.ts
+++ apps/web/src/app/admin-console/organizations/members/members.component.ts
@@ -215,11 +215,7 @@ export class MembersComponent extends BaseMembersComponent<OrganizationUserView>
             .find((p) => p.organizationId === this.organization.id);
           this.orgResetPasswordPolicyEnabled = resetPasswordPolicy?.enabled;
 
-          const billingMetadata = await this.billingApiService.getOrganizationBillingMetadata(
-            this.organization.id,
-          );
-
-          this.orgIsOnSecretsManagerStandalone = billingMetadata.isOnSecretsManagerStandalone;
+          this.orgIsOnSecretsManagerStandalone = false; // don't get billing metadata
 
           await this.load();
 
