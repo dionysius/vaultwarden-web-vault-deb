79a0b0d46db222f9b05d0460d82b063b99edff26
cyprain-okeke <108260115+cyprain-okeke@users.noreply.github.com>
2024-05-14 14:50:36 +0100

    [AC-1934] Clients: Create component to display provider subscription details (#9129)
    * initial commit
    
    * Make changes for provider billing details
    
    * replace the hardcoded values with real data
    
    * Apply discount on the displayed amount
    
    * Fix the design issues base on the new design changes
    
    * Fix the design space issue
    
    * Remove unnecessary If statements
    
    * Revert the change
    
    * Remove unnecessary If statements
    
    * Refactoring the discount calculation for easy understanding

--- bitwarden_license/bit-web/src/app/billing/providers/provider-subscription.component.ts
+++ bitwarden_license/bit-web/src/app/billing/providers/provider-subscription.component.ts
@@ -1,7 +1,86 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subject, concatMap, takeUntil } from "rxjs";
+
+import { BillingApiServiceAbstraction } from "@bitwarden/common/billing/abstractions/billilng-api.service.abstraction";
+import {
+  Plans,
+  ProviderSubscriptionResponse,
+} from "@bitwarden/common/billing/models/response/provider-subscription-response";
 
 @Component({
   selector: "app-provider-subscription",
   templateUrl: "./provider-subscription.component.html",
 })
-export class ProviderSubscriptionComponent {}
+export class ProviderSubscriptionComponent {
+  subscription: ProviderSubscriptionResponse;
+  providerId: string;
+  firstLoaded = false;
+  loading: boolean;
+  private destroy$ = new Subject<void>();
+  totalCost: number;
+  currentDate = new Date();
+
+  constructor(
+    private billingApiService: BillingApiServiceAbstraction,
+    private route: ActivatedRoute,
+  ) {}
+
+  async ngOnInit() {
+    this.route.params
+      .pipe(
+        concatMap(async (params) => {
+          this.providerId = params.providerId;
+          await this.load();
+          this.firstLoaded = true;
+        }),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+  }
+
+  get isExpired() {
+    return this.subscription.status !== "active";
+  }
+
+  async load() {
+    if (this.loading) {
+      return;
+    }
+    this.loading = true;
+    this.subscription = await this.billingApiService.getProviderSubscription(this.providerId);
+    this.totalCost =
+      ((100 - this.subscription.discountPercentage) / 100) * this.sumCost(this.subscription.plans);
+    this.loading = false;
+  }
+
+  getFormattedCost(
+    cost: number,
+    seatMinimum: number,
+    purchasedSeats: number,
+    discountPercentage: number,
+  ): number {
+    const costPerSeat = cost / (seatMinimum + purchasedSeats);
+    const discountedCost = costPerSeat - (costPerSeat * discountPercentage) / 100;
+    return discountedCost;
+  }
+
+  getFormattedPlanName(planName: string): string {
+    const spaceIndex = planName.indexOf(" ");
+    return planName.substring(0, spaceIndex);
+  }
+
+  getFormattedSeatCount(seatMinimum: number, purchasedSeats: number): string {
+    const totalSeats = seatMinimum + purchasedSeats;
+    return totalSeats > 1 ? totalSeats.toString() : "";
+  }
+
+  sumCost(plans: Plans[]): number {
+    return plans.reduce((acc, plan) => acc + plan.cost, 0);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
