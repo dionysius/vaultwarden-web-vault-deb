bed8239c928e7a1be7f941c783d77345a44dae86
Nick Krantz <125900171+nick-livefront@users.noreply.github.com>
2024-05-16 14:30:44 -0500

    [PM-6823] Vault Header Refresh (#9197)
    * add V2 variant to pop-out component
    
    * Add V2 version of the vault header
    
    * refactor IconButtonModule import
    
    * refactor IconButtonModule import
    
    * use feature flag to drive pop out redesign
    
    * use an anchor rather than a button
    
    * remove VaultFilterService and add todo comment
    
    * remove no-decoration class will be fixed by component library
    
    * bring back constructor

--- apps/browser/src/platform/popup/components/pop-out.component.ts
+++ apps/browser/src/platform/popup/components/pop-out.component.ts
@@ -2,7 +2,10 @@ import { CommonModule } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
+import { IconButtonModule } from "@bitwarden/components";
 
 import BrowserPopupUtils from "../browser-popup-utils";
 
@@ -10,14 +13,20 @@ import BrowserPopupUtils from "../browser-popup-utils";
   selector: "app-pop-out",
   templateUrl: "pop-out.component.html",
   standalone: true,
-  imports: [CommonModule, JslibModule],
+  imports: [CommonModule, JslibModule, IconButtonModule],
 })
 export class PopOutComponent implements OnInit {
   @Input() show = true;
+  useRefreshVariant = false;
 
-  constructor(private platformUtilsService: PlatformUtilsService) {}
+  constructor(
+    private platformUtilsService: PlatformUtilsService,
+    private configService: ConfigService,
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.useRefreshVariant = await this.configService.getFeatureFlag(FeatureFlag.ExtensionRefresh);
+
     if (this.show) {
       if (
         (BrowserPopupUtils.inSidebar(window) && this.platformUtilsService.isFirefox()) ||
