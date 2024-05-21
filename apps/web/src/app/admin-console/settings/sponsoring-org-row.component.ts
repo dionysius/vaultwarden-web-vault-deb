ead52698c370450982cdd6ef10707c1accb939d8
KiruthigaManivannan <162679756+KiruthigaManivannan@users.noreply.github.com>
2024-05-21 18:56:03 +0530

    AC-2402 Migrate sponsoring org row component (#8894)
    * AC-2402 Migrate sponsoring org row component
    
    * AC-2402 Migrate Sponsoring Org Row Component

--- apps/web/src/app/admin-console/settings/sponsoring-org-row.component.ts
+++ apps/web/src/app/admin-console/settings/sponsoring-org-row.component.ts
@@ -20,10 +20,7 @@ export class SponsoringOrgRowComponent implements OnInit {
   @Output() sponsorshipRemoved = new EventEmitter();
 
   statusMessage = "loading";
-  statusClass: "text-success" | "text-danger" = "text-success";
-
-  revokeSponsorshipPromise: Promise<any>;
-  resendEmailPromise: Promise<any>;
+  statusClass: "tw-text-success" | "tw-text-danger" = "tw-text-success";
 
   private locale = "";
 
@@ -48,20 +45,15 @@ export class SponsoringOrgRowComponent implements OnInit {
 
   async revokeSponsorship() {
     try {
-      this.revokeSponsorshipPromise = this.doRevokeSponsorship();
-      await this.revokeSponsorshipPromise;
+      await this.doRevokeSponsorship();
     } catch (e) {
       this.logService.error(e);
     }
-
-    this.revokeSponsorshipPromise = null;
   }
 
   async resendEmail() {
-    this.resendEmailPromise = this.apiService.postResendSponsorshipOffer(this.sponsoringOrg.id);
-    await this.resendEmailPromise;
+    await this.apiService.postResendSponsorshipOffer(this.sponsoringOrg.id);
     this.platformUtilsService.showToast("success", null, this.i18nService.t("emailSent"));
-    this.resendEmailPromise = null;
   }
 
   get isSentAwaitingSync() {
@@ -106,31 +98,31 @@ export class SponsoringOrgRowComponent implements OnInit {
         "revokeWhenExpired",
         formatDate(validUntil, "MM/dd/yyyy", this.locale),
       );
-      this.statusClass = "text-danger";
+      this.statusClass = "tw-text-danger";
     } else if (toDelete) {
       // They want to delete and we don't have a valid until date so we can
       // this should only happen on a self-hosted install
       this.statusMessage = this.i18nService.t("requestRemoved");
-      this.statusClass = "text-danger";
+      this.statusClass = "tw-text-danger";
     } else if (validUntil) {
       // They don't want to delete and they have a valid until date
       // that means they are actively sponsoring someone
       this.statusMessage = this.i18nService.t("active");
-      this.statusClass = "text-success";
+      this.statusClass = "tw-text-success";
     } else if (selfHosted && lastSyncDate) {
       // We are on a self-hosted install and it has been synced but we have not gotten
       // a valid until date so we can't know if they are actively sponsoring someone
       this.statusMessage = this.i18nService.t("sent");
-      this.statusClass = "text-success";
+      this.statusClass = "tw-text-success";
     } else if (!selfHosted) {
       // We are in cloud and all other status checks have been false therefore we have
       // sent the request but it hasn't been accepted yet
       this.statusMessage = this.i18nService.t("sent");
-      this.statusClass = "text-success";
+      this.statusClass = "tw-text-success";
     } else {
       // We are on a self-hosted install and we have not synced yet
       this.statusMessage = this.i18nService.t("requested");
-      this.statusClass = "text-success";
+      this.statusClass = "tw-text-success";
     }
   }
 }
