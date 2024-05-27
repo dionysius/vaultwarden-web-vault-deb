2e6280ce88274455155c464b05f1d1dcb7b22a17
Matt Gibson <mgibson@bitwarden.com>
2024-05-27 08:12:53 -0400

    Prefer UserKeyDefinition for user-scoped data (#9349)

--- apps/browser/src/tools/popup/services/key-definitions.ts
+++ apps/browser/src/tools/popup/services/key-definitions.ts
@@ -1,23 +1,25 @@
 import { Jsonify } from "type-fest";
 
-import { BROWSER_SEND_MEMORY, KeyDefinition } from "@bitwarden/common/platform/state";
+import { BROWSER_SEND_MEMORY, UserKeyDefinition } from "@bitwarden/common/platform/state";
 
 import { BrowserComponentState } from "../../../models/browserComponentState";
 import { BrowserSendComponentState } from "../../../models/browserSendComponentState";
 
-export const BROWSER_SEND_COMPONENT = new KeyDefinition<BrowserSendComponentState>(
+export const BROWSER_SEND_COMPONENT = new UserKeyDefinition<BrowserSendComponentState>(
   BROWSER_SEND_MEMORY,
   "browser_send_component",
   {
     deserializer: (obj: Jsonify<BrowserSendComponentState>) =>
       BrowserSendComponentState.fromJSON(obj),
+    clearOn: ["logout", "lock"],
   },
 );
 
-export const BROWSER_SEND_TYPE_COMPONENT = new KeyDefinition<BrowserComponentState>(
+export const BROWSER_SEND_TYPE_COMPONENT = new UserKeyDefinition<BrowserComponentState>(
   BROWSER_SEND_MEMORY,
   "browser_send_type_component",
   {
     deserializer: (obj: Jsonify<BrowserComponentState>) => BrowserComponentState.fromJSON(obj),
+    clearOn: ["logout", "lock"],
   },
 );
