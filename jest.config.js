de92720d7e8406c7984d7642e0d5296d5ce548d8
Daniel James Smith <2670567+djsmith85@users.noreply.github.com>
2024-05-30 21:38:31 +0200

    [PM-7289] Create generator libraries (#9432)
    * Create and register @bitwarden/generator-core
    
    - Create libs/tools/generator/core
    - Register new library within tsconfigs
    - Register new library within eslint.configs
    
    * Create and register @bitwarden/generator-extensions
    
    - Create libs/tools/generator/extensions
    - Register new library within tsconfigs
    - Register new library within eslint.configs
    
    * Create and register @bitwarden/generator-components
    
    - Create libs/tools/generator/components
    - Register new library within tsconfigs
    - Register new library within eslint.configs
    
    * Update libs/tools/generator/components/package.json
    
    Co-authored-by: ✨ Audrey ✨ <ajensen@bitwarden.com>
    
    * Update libs/tools/generator/extensions/package.json
    
    Co-authored-by: ✨ Audrey ✨ <ajensen@bitwarden.com>
    
    * Add empty barrel files for new libs
    
    * Fix extension of test.environment
    
    ---------
    
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>
    Co-authored-by: ✨ Audrey ✨ <ajensen@bitwarden.com>

--- jest.config.js
+++ jest.config.js
@@ -31,6 +31,7 @@ module.exports = {
     "<rootDir>/libs/common/jest.config.js",
     "<rootDir>/libs/components/jest.config.js",
     "<rootDir>/libs/tools/export/vault-export/vault-export-core/jest.config.js",
+    "<rootDir>/libs/tools/generator/core/jest.config.js",
     "<rootDir>/libs/importer/jest.config.js",
     "<rootDir>/libs/platform/jest.config.js",
     "<rootDir>/libs/node/jest.config.js",
