ce88038c0d1546023846470bf26ddd89da602004
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-06-17 11:10:00 -0400

    improve developer experience of generator extension symbols (#9690)

--- libs/tools/generator/extensions/src/index.ts
+++ libs/tools/generator/extensions/src/index.ts
@@ -2,3 +2,8 @@ export * as history from "./history";
 export * as legacyPassword from "./legacy-password";
 export * as legacyUsername from "./legacy-username";
 export * as navigation from "./navigation";
+
+export { GeneratorHistoryService } from "./history";
+export { GeneratorNavigationService } from "./navigation";
+export { PasswordGenerationServiceAbstraction } from "./legacy-password";
+export { UsernameGenerationServiceAbstraction } from "./legacy-username";
