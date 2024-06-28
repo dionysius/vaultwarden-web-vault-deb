f0673dd16e1d5784c66b8fabae3121fb725ac028
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2024-06-28 16:30:05 -0400

    PM-4950 - Fix hint and verify delete components that had the data in the wrong place (#9877)

--- apps/web/src/app/oss-routing.module.ts
+++ apps/web/src/app/oss-routing.module.ts
@@ -321,28 +321,28 @@ const routes: Routes = [
       {
         path: "verify-recover-delete",
         canActivate: [unauthGuardFn()],
+        data: {
+          pageTitle: "deleteAccount",
+          titleId: "deleteAccount",
+        } satisfies DataProperties & AnonLayoutWrapperData,
         children: [
           {
             path: "",
             component: VerifyRecoverDeleteComponent,
-            data: {
-              pageTitle: "deleteAccount",
-              titleId: "deleteAccount",
-            } satisfies DataProperties & AnonLayoutWrapperData,
           },
         ],
       },
       {
         path: "hint",
         canActivate: [unauthGuardFn()],
+        data: {
+          pageTitle: "passwordHint",
+          titleId: "passwordHint",
+        } satisfies DataProperties & AnonLayoutWrapperData,
         children: [
           {
             path: "",
             component: HintComponent,
-            data: {
-              pageTitle: "passwordHint",
-              titleId: "passwordHint",
-            } satisfies DataProperties & AnonLayoutWrapperData,
           },
           {
             path: "",
