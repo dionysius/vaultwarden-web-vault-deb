82d6b26b180924a3182ce0a7ba61cdc5a1f7c10e
Nick Krantz <125900171+nick-livefront@users.noreply.github.com>
2024-08-01 16:11:52 -0500

    [PM-9839] Extension - Safari - Disable Account Switching (#10364)
    * only show account switching when enableAccountSwitching is true
    
    * update the title of the account switcher component
    
    * only show "Lock All" when more than one account is present
    
    * implement account switching restrictions on non-extension refresh page

--- apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
+++ apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, Location } from "@angular/common";
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Subject, firstValueFrom, map, of, switchMap, takeUntil } from "rxjs";
+import { Subject, firstValueFrom, map, of, startWith, switchMap, takeUntil } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { VaultTimeoutSettingsService } from "@bitwarden/common/abstractions/vault-timeout/vault-timeout-settings.service";
@@ -22,6 +22,7 @@ import {
   SectionHeaderComponent,
 } from "@bitwarden/components";
 
+import { enableAccountSwitching } from "../../../platform/flags";
 import { PopOutComponent } from "../../../platform/popup/components/pop-out.component";
 import { HeaderComponent } from "../../../platform/popup/header.component";
 import { PopupHeaderComponent } from "../../../platform/popup/layout/popup-header.component";
@@ -57,6 +58,7 @@ export class AccountSwitcherComponent implements OnInit, OnDestroy {
   loading = false;
   activeUserCanLock = false;
   extensionRefreshFlag = false;
+  enableAccountSwitching = true;
 
   constructor(
     private accountSwitcherService: AccountSwitcherService,
@@ -87,7 +89,24 @@ export class AccountSwitcherComponent implements OnInit, OnDestroy {
     ),
   );
 
+  readonly showLockAll$ = this.availableAccounts$.pipe(
+    startWith([]),
+    map((accounts) => accounts.filter((a) => !a.isActive)),
+    switchMap((accounts) => {
+      // If account switching is disabled, don't show the lock all button
+      // as only one account should be shown.
+      if (!enableAccountSwitching()) {
+        return of(false);
+      }
+
+      // When there are an inactive accounts provide the option to lock all accounts
+      // Note: "Add account" is counted as an inactive account, so check for more than one account
+      return of(accounts.length > 1);
+    }),
+  );
+
   async ngOnInit() {
+    this.enableAccountSwitching = enableAccountSwitching();
     this.extensionRefreshFlag = await this.configService.getFeatureFlag(
       FeatureFlag.ExtensionRefresh,
     );
