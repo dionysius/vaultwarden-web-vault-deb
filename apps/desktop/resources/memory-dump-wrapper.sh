f997a094f75b5ae26c35bd194205590064fbdc46
Bernd Schoolmann <mail@quexten.com>
2024-08-07 16:15:31 +0200

    [PM-10689] Fix rpm (& deb) packages not starting by following symlink (#10429)
    * Fix rpm packages not starting by following wrapper-script executable symlink
    
    * Restore newline
    
    * Add missing space

--- apps/desktop/resources/memory-dump-wrapper.sh
+++ apps/desktop/resources/memory-dump-wrapper.sh
@@ -3,6 +3,10 @@
 # disable core dumps
 ulimit -c 0
 
-APP_PATH=$(dirname "$0")
+# might be behind symlink
+RAW_PATH=$(readlink -f "$0")
+APP_PATH=$(dirname $RAW_PATH)
+
 # pass through all args
 $APP_PATH/bitwarden-app "$@"
+
