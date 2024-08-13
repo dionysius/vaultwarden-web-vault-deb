2acd2017520645810b3e419a6a91350167cb1799
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2024-08-13 12:14:15 -0400

    BRE-243 - Fix credentials used for Desktop upload to App Store (#10503)

--- apps/desktop/scripts/after-sign.js
+++ apps/desktop/scripts/after-sign.js
@@ -50,14 +50,27 @@ async function run(context) {
 
   if (macBuild) {
     console.log("### Notarizing " + appPath);
-    const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
-    const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
-    return await notarize({
-      tool: "notarytool",
-      appPath: appPath,
-      teamId: "LTZ2PFU5D6",
-      appleId: appleId,
-      appleIdPassword: appleIdPassword,
-    });
+    if (process.env.APP_STORE_CONNECT_TEAM_ISSUER) {
+      const appleApiIssuer = process.env.APP_STORE_CONNECT_TEAM_ISSUER;
+      const appleApiKey = process.env.APP_STORE_CONNECT_AUTH_KEY_PATH;
+      const appleApiKeyId = process.env.APP_STORE_CONNECT_AUTH_KEY;
+      return await notarize({
+        tool: "notarytool",
+        appPath: appPath,
+        appleApiIssuer: appleApiIssuer,
+        appleApiKey: appleApiKey,
+        appleApiKeyId: appleApiKeyId,
+      });
+    } else {
+      const appleId = process.env.APPLE_ID_USERNAME || process.env.APPLEID;
+      const appleIdPassword = process.env.APPLE_ID_PASSWORD || `@keychain:AC_PASSWORD`;
+      return await notarize({
+        tool: "notarytool",
+        appPath: appPath,
+        teamId: "LTZ2PFU5D6",
+        appleId: appleId,
+        appleIdPassword: appleIdPassword,
+      });
+    }
   }
 }
