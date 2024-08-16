4d088f38565a9c830ac09cbaf59f9b5e264be3c7
Oscar Hinton <Hinton@users.noreply.github.com>
2024-08-16 15:40:00 +0200

    Fix desktop broken on main (#10540)

--- apps/desktop/src/vault/app/accounts/premium.component.ts
+++ apps/desktop/src/vault/app/accounts/premium.component.ts
@@ -33,7 +33,6 @@ export class PremiumComponent extends BasePremiumComponent {
       apiService,
       configService,
       logService,
-      stateService,
       dialogService,
       environmentService,
       billingAccountProfileStateService,
