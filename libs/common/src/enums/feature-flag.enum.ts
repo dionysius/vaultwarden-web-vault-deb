1ccc4508fb02af3ea1929dd72af0c26e28886122
Addison Beck <github@addisonbeck.com>
2024-08-16 05:05:40 -0400

    Add `AccountDeprovisioning` feature flag (#10531)

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -32,6 +32,7 @@ export enum FeatureFlag {
   EnableUpgradePasswordManagerSub = "AC-2708-upgrade-password-manager-sub",
   GenerateIdentityFillScriptRefactor = "generate-identity-fill-script-refactor",
   DelayFido2PageScriptInitWithinMv2 = "delay-fido2-page-script-init-within-mv2",
+  AccountDeprovisioning = "pm-10308-account-deprovisioning",
 }
 
 export type AllowedFeatureFlagTypes = boolean | number | string;
@@ -74,6 +75,7 @@ export const DefaultFeatureFlagValue = {
   [FeatureFlag.EnableUpgradePasswordManagerSub]: FALSE,
   [FeatureFlag.GenerateIdentityFillScriptRefactor]: FALSE,
   [FeatureFlag.DelayFido2PageScriptInitWithinMv2]: FALSE,
+  [FeatureFlag.AccountDeprovisioning]: FALSE,
 } satisfies Record<FeatureFlag, AllowedFeatureFlagTypes>;
 
 export type DefaultFeatureFlagValueType = typeof DefaultFeatureFlagValue;
