1c43402601b35eaa7ba92f5d1212bdeb1d71e4c3
Alec Rippberger <127791530+alec-livefront@users.noreply.github.com>
2024-08-29 17:09:20 -0500

    Check result is not null or undefined before trying to access the action property. (#10754)

--- apps/web/src/app/vault/individual-vault/vault.component.ts
+++ apps/web/src/app/vault/individual-vault/vault.component.ts
@@ -718,12 +718,12 @@ export class VaultComponent implements OnInit, OnDestroy {
     const result: ViewCipherDialogCloseResult = await lastValueFrom(dialogRef.closed);
 
     // If the dialog was closed by deleting the cipher, refresh the vault.
-    if (result.action === ViewCipherDialogResult.deleted) {
+    if (result?.action === ViewCipherDialogResult.deleted) {
       this.refresh();
     }
 
     // If the dialog was closed by any other action (close button, escape key, etc), navigate back to the vault.
-    if (!result.action) {
+    if (!result?.action) {
       this.go({ cipherId: null, itemId: null, action: null });
     }
   }
