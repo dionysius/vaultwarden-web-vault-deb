45806ed8d82f07b421467ab99f65a550d641434c
Bernd Schoolmann <mail@quexten.com>
2024-09-06 19:00:50 +0200

    Add fallbackbulkencryptservice to main.background.ts (#10696)

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -782,6 +782,8 @@ export default class MainBackground {
       this.authService,
     );
 
+    this.bulkEncryptService = new FallbackBulkEncryptService(this.encryptService);
+
     this.cipherService = new CipherService(
       this.cryptoService,
       this.domainSettingsService,
