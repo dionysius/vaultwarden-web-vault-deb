d4767bf1724aae9d6a3719be044fc298d58ccc21
Jordan Aasen <166539328+jaasen-livefront@users.noreply.github.com>
2024-10-04 06:35:07 -0700

    revert change to removing send tab (#11394)

--- apps/browser/src/platform/popup/layout/popup-tab-navigation.component.ts
+++ apps/browser/src/platform/popup/layout/popup-tab-navigation.component.ts
@@ -1,40 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { RouterModule } from "@angular/router";
-import { filter, map, switchMap } from "rxjs";
 
-import { PolicyService } from "@bitwarden/common/admin-console/abstractions/policy/policy.service.abstraction";
-import { PolicyType } from "@bitwarden/common/admin-console/enums";
-import { SendService } from "@bitwarden/common/tools/send/services/send.service.abstraction";
 import { LinkModule } from "@bitwarden/components";
-
-const allNavButtons = [
-  {
-    label: "Vault",
-    page: "/tabs/vault",
-    iconKey: "lock",
-    iconKeyActive: "lock-f",
-  },
-  {
-    label: "Generator",
-    page: "/tabs/generator",
-    iconKey: "generate",
-    iconKeyActive: "generate-f",
-  },
-  {
-    label: "Send",
-    page: "/tabs/send",
-    iconKey: "send",
-    iconKeyActive: "send-f",
-  },
-  {
-    label: "Settings",
-    page: "/tabs/settings",
-    iconKey: "cog",
-    iconKeyActive: "cog-f",
-  },
-];
 
 @Component({
   selector: "popup-tab-navigation",
@@ -46,23 +14,30 @@ const allNavButtons = [
   },
 })
 export class PopupTabNavigationComponent {
-  navButtons = allNavButtons;
-  constructor(
-    private policyService: PolicyService,
-    private sendService: SendService,
-  ) {
-    this.policyService
-      .policyAppliesToActiveUser$(PolicyType.DisableSend)
-      .pipe(
-        filter((policyAppliesToActiveUser) => policyAppliesToActiveUser),
-        switchMap(() => this.sendService.sends$),
-        map((sends) => sends.length > 0),
-        takeUntilDestroyed(),
-      )
-      .subscribe((hasSends) => {
-        this.navButtons = hasSends
-          ? allNavButtons
-          : allNavButtons.filter((b) => b.page !== "/tabs/send");
-      });
-  }
+  navButtons = [
+    {
+      label: "Vault",
+      page: "/tabs/vault",
+      iconKey: "lock",
+      iconKeyActive: "lock-f",
+    },
+    {
+      label: "Generator",
+      page: "/tabs/generator",
+      iconKey: "generate",
+      iconKeyActive: "generate-f",
+    },
+    {
+      label: "Send",
+      page: "/tabs/send",
+      iconKey: "send",
+      iconKeyActive: "send-f",
+    },
+    {
+      label: "Settings",
+      page: "/tabs/settings",
+      iconKey: "cog",
+      iconKeyActive: "cog-f",
+    },
+  ];
 }
