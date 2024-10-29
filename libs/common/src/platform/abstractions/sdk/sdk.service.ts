021efa2c90be060bb66aec01aefd00068a50abdf
Andreas Coroiu <acoroiu@bitwarden.com>
2024-10-29 16:00:02 +0100

    Document `firstValueFrom` not being supported (#11769)

--- libs/common/src/platform/abstractions/sdk/sdk.service.ts
+++ libs/common/src/platform/abstractions/sdk/sdk.service.ts
@@ -20,6 +20,11 @@ export abstract class SdkService {
    * Retrieve a client initialized for a specific user.
    * This client can be used for operations that require a user context, such as retrieving ciphers
    * and operations involving crypto. It can also be used for operations that don't require a user context.
+   *
+   * **WARNING:** Do not use `firstValueFrom(userClient$)`! Any operations on the client must be done within the observable.
+   * The client will be destroyed when the observable is no longer subscribed to.
+   * Please let platform know if you need a client that is not destroyed when the observable is no longer subscribed to.
+   *
    * @param userId
    */
   abstract userClient$(userId: UserId): Observable<BitwardenClient>;
