af6a2f5553a765d94702fdcd1dd0e1beb5bc76c6
Oscar Hinton <Hinton@users.noreply.github.com>
2024-11-05 10:54:38 +0100

    [PM-13375] Gulp: Remove beta builds (#11482)
    Part of the browser build script refactor effort. bitwarden.atlassian.net/browse/PM-6683
    
    The beta logic adds a fair bit of complexity and is currently unused. Let’s remove it and we can look into re-add it after migrating to our new build system.

--- apps/browser/gulpfile.js
+++ apps/browser/gulpfile.js
@@ -9,7 +9,6 @@ const replace = require("gulp-replace");
 
 const manifest = require("./src/manifest.json");
 const manifestVersion = parseInt(process.env.MANIFEST_VERSION || manifest.version);
-const betaBuild = process.env.BETA_BUILD === "1";
 
 const paths = {
   build: "./build/",
@@ -17,26 +16,10 @@ const paths = {
   safari: "./src/safari/",
 };
 
-/**
- * Converts a number to a tuple containing two Uint16's
- * @param num {number} This number is expected to be a integer style number with no decimals
- *
- * @returns {number[]} A tuple containing two elements that are both numbers.
- */
-function numToUint16s(num) {
-  var arr = new ArrayBuffer(4);
-  var view = new DataView(arr);
-  view.setUint32(0, num, false);
-  return [view.getUint16(0), view.getUint16(2)];
-}
-
 function buildString() {
   var build = "";
   if (process.env.MANIFEST_VERSION) {
     build = `-mv${process.env.MANIFEST_VERSION}`;
-  }
-  if (betaBuild) {
-    build += "-beta";
   }
   if (process.env.BUILD_NUMBER && process.env.BUILD_NUMBER !== "") {
     build = `-${process.env.BUILD_NUMBER}`;
@@ -71,9 +54,6 @@ function distFirefox() {
     manifest.optional_permissions = manifest.optional_permissions.filter(
       (permission) => permission !== "privacy",
     );
-    if (betaBuild) {
-      manifest = applyBetaLabels(manifest);
-    }
     return manifest;
   });
 }
@@ -90,9 +70,6 @@ function distOpera() {
       delete manifest.commands._execute_sidebar_action;
     }
 
-    if (betaBuild) {
-      manifest = applyBetaLabels(manifest);
-    }
     return manifest;
   });
 }
@@ -102,9 +79,6 @@ function distChrome() {
     delete manifest.applications;
     delete manifest.sidebar_action;
     delete manifest.commands._execute_sidebar_action;
-    if (betaBuild) {
-      manifest = applyBetaLabels(manifest);
-    }
     return manifest;
   });
 }
@@ -114,9 +88,6 @@ function distEdge() {
     delete manifest.applications;
     delete manifest.sidebar_action;
     delete manifest.commands._execute_sidebar_action;
-    if (betaBuild) {
-      manifest = applyBetaLabels(manifest);
-    }
     return manifest;
   });
 }
@@ -237,9 +208,6 @@ async function safariCopyBuild(source, dest) {
             delete manifest.commands._execute_sidebar_action;
             delete manifest.optional_permissions;
             manifest.permissions.push("nativeMessaging");
-            if (betaBuild) {
-              manifest = applyBetaLabels(manifest);
-            }
             return manifest;
           }),
         ),
@@ -252,30 +220,6 @@ async function safariCopyBuild(source, dest) {
 function stdOutProc(proc) {
   proc.stdout.on("data", (data) => console.log(data.toString()));
   proc.stderr.on("data", (data) => console.error(data.toString()));
-}
-
-function applyBetaLabels(manifest) {
-  manifest.name = "Bitwarden Password Manager BETA";
-  manifest.short_name = "Bitwarden BETA";
-  manifest.description = "THIS EXTENSION IS FOR BETA TESTING BITWARDEN.";
-  if (process.env.GITHUB_RUN_ID) {
-    const existingVersionParts = manifest.version.split("."); // 3 parts expected 2024.4.0
-
-    // GITHUB_RUN_ID is a number like: 8853654662
-    // which will convert to [ 4024, 3206 ]
-    // and a single incremented id of 8853654663 will become  [ 4024, 3207 ]
-    const runIdParts = numToUint16s(parseInt(process.env.GITHUB_RUN_ID));
-
-    // Only use the first 2 parts from the given version number and base the other 2 numbers from the GITHUB_RUN_ID
-    // Example: 2024.4.4024.3206
-    const betaVersion = `${existingVersionParts[0]}.${existingVersionParts[1]}.${runIdParts[0]}.${runIdParts[1]}`;
-
-    manifest.version_name = `${betaVersion} beta - ${process.env.GITHUB_SHA.slice(0, 8)}`;
-    manifest.version = betaVersion;
-  } else {
-    manifest.version = `${manifest.version}.0`;
-  }
-  return manifest;
 }
 
 exports["dist:firefox"] = distFirefox;
