e206fc18195b74c5bb0b4e14a9b8d15dc2a59126
Thomas Avery <43214426+Thomas-Avery@users.noreply.github.com>
2024-11-05 10:03:43 -0600

    [PM-14478] Add libs/key-management to jest.config (#11854)

--- jest.config.js
+++ jest.config.js
@@ -41,6 +41,7 @@ module.exports = {
     "<rootDir>/libs/platform/jest.config.js",
     "<rootDir>/libs/node/jest.config.js",
     "<rootDir>/libs/vault/jest.config.js",
+    "<rootDir>/libs/key-management/jest.config.js",
   ],
 
   // Workaround for a memory leak that crashes tests in CI:
