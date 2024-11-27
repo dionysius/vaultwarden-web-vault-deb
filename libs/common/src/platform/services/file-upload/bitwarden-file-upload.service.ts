f71d42e051c536d7edafc205e1027a10d1b0148b
Andreas Coroiu <acoroiu@bitwarden.com>
2024-11-27 12:00:46 +0100

    fix: lint after PM-13923 (#12164)

--- libs/common/src/platform/services/file-upload/bitwarden-file-upload.service.ts
+++ libs/common/src/platform/services/file-upload/bitwarden-file-upload.service.ts
@@ -9,21 +9,21 @@ export class BitwardenFileUploadService {
   ) {
     const fd = new FormData();
 
-      if (Utils.isBrowser) {
-        const blob = new Blob([encryptedFileData.buffer], { type: "application/octet-stream" });
-        fd.append("data", blob, encryptedFileName);
-      } else if (Utils.isNode) {
-        fd.append(
-          "data",
-          Buffer.from(encryptedFileData.buffer) as any,
-          {
-            filename: encryptedFileName,
-            contentType: "application/octet-stream",
-          } as any,
-        );
-      } else {
-        throw new Error("Unsupported environment");
-      }
+    if (Utils.isBrowser) {
+      const blob = new Blob([encryptedFileData.buffer], { type: "application/octet-stream" });
+      fd.append("data", blob, encryptedFileName);
+    } else if (Utils.isNode) {
+      fd.append(
+        "data",
+        Buffer.from(encryptedFileData.buffer) as any,
+        {
+          filename: encryptedFileName,
+          contentType: "application/octet-stream",
+        } as any,
+      );
+    } else {
+      throw new Error("Unsupported environment");
+    }
 
     await apiCall(fd);
   }
