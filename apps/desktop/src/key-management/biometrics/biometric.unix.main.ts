92a620dd9c06c46127164d3c7a103aeafff92708
Bernd Schoolmann <mail@quexten.com>
2024-12-11 07:10:06 -0800

    [BEEEP/PM-10534] Add snap biometric support (#12187)
    * Add snap biometric support
    
    * Fix linting
    
    * Remove unused message
    
    * Disable snap browser integration again

--- apps/desktop/src/key-management/biometrics/biometric.unix.main.ts
+++ apps/desktop/src/key-management/biometrics/biometric.unix.main.ts
@@ -87,8 +87,8 @@ export default class BiometricUnixMain implements OsBiometricService {
   }
 
   async authenticateBiometric(): Promise<boolean> {
-    const hwnd = this.windowMain.win.getNativeWindowHandle();
-    return await biometrics.prompt(hwnd, this.i18nservice.t("polkitConsentMessage"));
+    const hwnd = Buffer.from("");
+    return await biometrics.prompt(hwnd, "");
   }
 
   async osSupportsBiometric(): Promise<boolean> {
@@ -98,10 +98,14 @@ export default class BiometricUnixMain implements OsBiometricService {
     // This could be dynamically detected on dbus in the future.
     // We should check if a libsecret implementation is available on the system
     // because otherwise we cannot offlod the protected userkey to secure storage.
-    return (await passwords.isAvailable()) && !isSnapStore();
+    return await passwords.isAvailable();
   }
 
   async osBiometricsNeedsSetup(): Promise<boolean> {
+    if (isSnapStore()) {
+      return false;
+    }
+
     // check whether the polkit policy is loaded via dbus call to polkit
     return !(await biometrics.available());
   }
