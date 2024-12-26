b3155d19dda3cf35044a55e9aebfb9e72aeec967
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-12-26 15:25:37 -0500

    Hide Open At Login Option On Windows Store Installs (#12112)

--- apps/desktop/src/app/accounts/settings.component.ts
+++ apps/desktop/src/app/accounts/settings.component.ts
@@ -58,6 +58,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
   requireEnableTray = false;
   showDuckDuckGoIntegrationOption = false;
   showSshAgentOption = false;
+  showOpenAtLoginOption = false;
   isWindows: boolean;
   isLinux: boolean;
 
@@ -165,6 +166,8 @@ export class SettingsComponent implements OnInit, OnDestroy {
     const startToTrayKey = isMac ? "startToMenuBar" : "startToTray";
     this.startToTrayText = this.i18nService.t(startToTrayKey);
     this.startToTrayDescText = this.i18nService.t(startToTrayKey + "Desc");
+
+    this.showOpenAtLoginOption = !ipc.platform.isWindowsStore;
 
     // DuckDuckGo browser is only for macos initially
     this.showDuckDuckGoIntegrationOption = isMac;
