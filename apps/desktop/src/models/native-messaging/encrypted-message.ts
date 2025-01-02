6f7883175deae147acdd28bc63f66cc4eac5f8d2
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 19:05:42 +0100

    remove other clients

--- apps/desktop/src/models/native-messaging/encrypted-message.ts
+++
@@ -1,8 +0,0 @@
-import { EncString } from "@bitwarden/common/platform/models/domain/enc-string";
-
-import { MessageCommon } from "./message-common";
-
-export type EncryptedMessage = MessageCommon & {
-  // Will decrypt to a DecryptedCommandData object
-  encryptedCommand: EncString;
-};
