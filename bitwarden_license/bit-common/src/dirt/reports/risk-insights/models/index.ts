5337d20218bc8dd17a9fd8963c6c13be744da20f
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-01-02 18:41:19 +0100

    remove non-free code from bitwarden_license

--- bitwarden_license/bit-common/src/dirt/reports/risk-insights/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./api-models.types";
-export * from "./password-health";
-export * from "./report-data-service.types";
-export * from "./report-encryption.types";
-export * from "./report-models";
-export * from "./drawer-models.types";
