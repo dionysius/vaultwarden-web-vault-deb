c349ea95c6a2608bee3296a60805a98e3d24348f
Daniel James Smith <2670567+djsmith85@users.noreply.github.com>
2025-01-06 18:11:31 +0100

    Remove v1 popout component (#12518)
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>

--- apps/browser/src/platform/popup/components/pop-out.component.ts
+++ apps/browser/src/platform/popup/components/pop-out.component.ts
@@ -2,8 +2,6 @@ import { CommonModule } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
-import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { IconButtonModule } from "@bitwarden/components";
 
@@ -17,16 +15,10 @@ import BrowserPopupUtils from "../browser-popup-utils";
 })
 export class PopOutComponent implements OnInit {
   @Input() show = true;
-  useRefreshVariant = false;
 
-  constructor(
-    private platformUtilsService: PlatformUtilsService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private platformUtilsService: PlatformUtilsService) {}
 
   async ngOnInit() {
-    this.useRefreshVariant = await this.configService.getFeatureFlag(FeatureFlag.ExtensionRefresh);
-
     if (this.show) {
       if (
         (BrowserPopupUtils.inSidebar(window) && this.platformUtilsService.isFirefox()) ||
