c1e3836cc32ad524bc4ab739375d07dcc63bdd4c
Rui Tomé <108268980+r-tome@users.noreply.github.com>
2025-01-10 11:16:51 +0000

    [PM-13758] Ensure decrypted cipherId is used for event collection in get command (#12459)

--- apps/cli/src/commands/get.command.ts
+++ apps/cli/src/commands/get.command.ts
@@ -152,11 +152,9 @@ export class GetCommand extends DownloadCommand {
       }
     }
 
-    // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.eventCollectionService.collect(
+    await this.eventCollectionService.collect(
       EventType.Cipher_ClientViewed,
-      id,
+      decCipher.id,
       true,
       decCipher.organizationId,
     );
