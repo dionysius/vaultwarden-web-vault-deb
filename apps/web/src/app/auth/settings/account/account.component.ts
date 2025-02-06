3e988fae6f554fdb11d881eedfdd4058f619753d
Todd Martin <106564991+trmartin4@users.noreply.github.com>
2025-02-06 14:37:08 -0500

    Remove feature flag check from opt-out (#13130)

--- apps/web/src/app/auth/settings/account/account.component.ts
+++ apps/web/src/app/auth/settings/account/account.component.ts
@@ -34,7 +34,6 @@ export class AccountComponent implements OnInit, OnDestroy {
   showChangeEmail$: Observable<boolean> = new Observable();
   showPurgeVault$: Observable<boolean> = new Observable();
   showDeleteAccount$: Observable<boolean> = new Observable();
-  showSetNewDeviceLoginProtection$: Observable<boolean> = new Observable();
   verifyNewDeviceLogin: boolean = true;
 
   constructor(
@@ -48,9 +47,6 @@ export class AccountComponent implements OnInit, OnDestroy {
   async ngOnInit() {
     const userId = await firstValueFrom(getUserId(this.accountService.activeAccount$));
 
-    this.showSetNewDeviceLoginProtection$ = this.configService.getFeatureFlag$(
-      FeatureFlag.NewDeviceVerification,
-    );
     const isAccountDeprovisioningEnabled$ = this.configService.getFeatureFlag$(
       FeatureFlag.AccountDeprovisioning,
     );
