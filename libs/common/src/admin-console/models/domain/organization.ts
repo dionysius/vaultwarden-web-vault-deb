eadf0cbaa1aeaf4c5e4089718456e23cf2f84000
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-03-05 08:30:43 +0100

    disable integrations

--- libs/common/src/admin-console/models/domain/organization.ts
+++ libs/common/src/admin-console/models/domain/organization.ts
@@ -371,6 +371,7 @@ export class Organization {
   }
 
   get canAccessIntegrations() {
+    return false; // disable integrations for Vaultwarden
     return (
       (this.productTierType === ProductTierType.Teams ||
         this.productTierType === ProductTierType.Enterprise) &&
