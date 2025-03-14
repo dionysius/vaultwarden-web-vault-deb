fdcb6ff7a2fbefeccbaf5b5bcd4243f8fb84c09a
Jordan Aasen <166539328+jaasen-livefront@users.noreply.github.com>
2025-03-14 15:37:44 -0700

    remove bulk share dialog components and related modules and i18n keys (#13815)

--- apps/web/src/app/vault/individual-vault/bulk-action-dialogs/bulk-dialogs.module.ts
+++ apps/web/src/app/vault/individual-vault/bulk-action-dialogs/bulk-dialogs.module.ts
@@ -4,11 +4,10 @@ import { SharedModule } from "../../../shared";
 
 import { BulkDeleteDialogComponent } from "./bulk-delete-dialog/bulk-delete-dialog.component";
 import { BulkMoveDialogComponent } from "./bulk-move-dialog/bulk-move-dialog.component";
-import { BulkShareDialogComponent } from "./bulk-share-dialog/bulk-share-dialog.component";
 
 @NgModule({
   imports: [SharedModule],
-  declarations: [BulkDeleteDialogComponent, BulkMoveDialogComponent, BulkShareDialogComponent],
-  exports: [BulkDeleteDialogComponent, BulkMoveDialogComponent, BulkShareDialogComponent],
+  declarations: [BulkDeleteDialogComponent, BulkMoveDialogComponent],
+  exports: [BulkDeleteDialogComponent, BulkMoveDialogComponent],
 })
 export class BulkDialogsModule {}
