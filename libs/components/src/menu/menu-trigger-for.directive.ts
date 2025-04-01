b0ccadcc0f11594351484a2b6eab5556b8580c38
Jordan Aasen <166539328+jaasen-livefront@users.noreply.github.com>
2025-04-01 14:15:54 -0700

    fix menu trigger fallback position (#14040)

--- libs/components/src/menu/menu-trigger-for.directive.ts
+++ libs/components/src/menu/menu-trigger-for.directive.ts
@@ -48,11 +48,12 @@ export class MenuTriggerForDirective implements OnDestroy {
           overlayX: "start",
           overlayY: "top",
         },
+        // Fallback position: show above the trigger
         {
-          originX: "end",
-          originY: "bottom",
-          overlayX: "end",
-          overlayY: "top",
+          originX: "start",
+          originY: "top",
+          overlayX: "start",
+          overlayY: "bottom",
         },
       ])
       .withLockedPosition(true)
