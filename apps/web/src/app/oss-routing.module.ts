3998c0692fd14f75fb90c68ead403b26a4c0b73d
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-04-16 12:54:56 +0200

    remove bitwarden from login page

--- apps/web/src/app/oss-routing.module.ts
+++ apps/web/src/app/oss-routing.module.ts
@@ -220,7 +220,7 @@ const routes: Routes = [
         canActivate: [unauthGuardFn()],
         data: {
           pageTitle: {
-            key: "logInToBitwarden",
+            key: "logIn",
           },
           pageIcon: VaultIcon,
         } satisfies RouteDataProperties & AnonLayoutWrapperData,
