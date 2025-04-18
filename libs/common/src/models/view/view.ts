9d16435d0858d40783ff8bc5494f27314cf00a0f
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2025-04-18 09:52:12 -0400

    docs(ViewModel): Add JSDocs to view to explain proper use (#14214)

--- libs/common/src/models/view/view.ts
+++ libs/common/src/models/view/view.ts
@@ -1,1 +1,5 @@
+// See https://contributing.bitwarden.com/architecture/clients/data-model/#view for proper use.
+// View models represent the decrypted state of a corresponding Domain model.
+// They typically match the Domain model but contains a decrypted string for any EncString fields.
+// Don't use this to represent arbitrary component view data as that isn't what it is for.
 export class View {}
