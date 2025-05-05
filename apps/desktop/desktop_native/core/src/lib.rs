a1e975a6aec198b5ae4ddf6f7868e195c01bbaa4
Bernd Schoolmann <mail@quexten.com>
2025-05-05 12:45:56 +0200

    [BEEEP] Add zeroizing alloc to desktop_native (#14601)
    * Add zeroizing alloc
    
    * Cleanup
    
    * Cleanup

--- apps/desktop/desktop_native/core/src/lib.rs
+++ apps/desktop/desktop_native/core/src/lib.rs
@@ -8,3 +8,8 @@ pub mod password;
 pub mod powermonitor;
 pub mod process_isolation;
 pub mod ssh_agent;
+
+use zeroizing_alloc::ZeroAlloc;
+
+#[global_allocator]
+static ALLOC: ZeroAlloc<std::alloc::System> = ZeroAlloc(std::alloc::System);
