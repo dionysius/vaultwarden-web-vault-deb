0e0be0a3decf2ae93976ebf6257086a09de718c2
Addison Beck <github@addisonbeck.com>
2025-05-23 09:07:32 -0400

    ignore one eslint error (#14896)

--- libs/vault/src/components/assign-collections.component.spec.ts
+++ libs/vault/src/components/assign-collections.component.spec.ts
@@ -3,6 +3,8 @@ import { By } from "@angular/platform-browser";
 import { mock } from "jest-mock-extended";
 import { of } from "rxjs";
 
+// This import has been flagged as unallowed for this class. It may be involved in a circular dependency loop.
+// eslint-disable-next-line no-restricted-imports
 import { CollectionService, CollectionView } from "@bitwarden/admin-console/common";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
