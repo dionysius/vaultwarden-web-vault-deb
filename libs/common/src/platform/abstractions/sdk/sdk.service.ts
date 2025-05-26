beb00a206b7b48f4f053b394b06160b5dd2c3f2c
Oscar Hinton <Hinton@users.noreply.github.com>
2025-05-26 17:02:28 +0200

    Add UUID helpers to the SDK (#14939)
    * Add UUID helpers to the SDK
    
    * Address review feedback

--- libs/common/src/platform/abstractions/sdk/sdk.service.ts
+++ libs/common/src/platform/abstractions/sdk/sdk.service.ts
@@ -1,14 +1,39 @@
 import { Observable } from "rxjs";
 
-import { BitwardenClient } from "@bitwarden/sdk-internal";
+import { BitwardenClient, Uuid } from "@bitwarden/sdk-internal";
 
 import { UserId } from "../../../types/guid";
 import { Rc } from "../../misc/reference-counting/rc";
+import { Utils } from "../../misc/utils";
 
 export class UserNotLoggedInError extends Error {
   constructor(userId: UserId) {
     super(`User (${userId}) is not logged in`);
   }
+}
+
+export class InvalidUuid extends Error {
+  constructor(uuid: string) {
+    super(`Invalid UUID: ${uuid}`);
+  }
+}
+
+/**
+ * Converts a string to UUID. Will throw an error if the UUID is non valid.
+ */
+export function asUuid<T extends Uuid>(uuid: string): T {
+  if (Utils.isGuid(uuid)) {
+    return uuid as T;
+  }
+
+  throw new InvalidUuid(uuid);
+}
+
+/**
+ * Converts a UUID to the string representation.
+ */
+export function uuidToString<T extends Uuid>(uuid: T): string {
+  return uuid as unknown as string;
 }
 
 export abstract class SdkService {
