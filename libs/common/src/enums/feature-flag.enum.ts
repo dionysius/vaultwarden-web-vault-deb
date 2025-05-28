169fdd5883352acae4fcc976d4122da5e9ea0a29
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2025-05-28 10:56:44 -0400

    [PM-20650] Feature flag addition to clients (#14824)
    * Feature flag addition to clients
    
    * Updating feature flag name

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -59,6 +59,7 @@ export enum FeatureFlag {
   CipherKeyEncryption = "cipher-key-encryption",
   PM18520_UpdateDesktopCipherForm = "pm-18520-desktop-cipher-forms",
   EndUserNotifications = "pm-10609-end-user-notifications",
+  RemoveCardItemTypePolicy = "pm-16442-remove-card-item-type-policy",
 
   /* Platform */
   IpcChannelFramework = "ipc-channel-framework",
@@ -107,6 +108,7 @@ export const DefaultFeatureFlagValue = {
   [FeatureFlag.PM18520_UpdateDesktopCipherForm]: FALSE,
   [FeatureFlag.EndUserNotifications]: FALSE,
   [FeatureFlag.PM19941MigrateCipherDomainToSdk]: FALSE,
+  [FeatureFlag.RemoveCardItemTypePolicy]: FALSE,
 
   /* Auth */
   [FeatureFlag.PM16117_ChangeExistingPasswordRefactor]: FALSE,
