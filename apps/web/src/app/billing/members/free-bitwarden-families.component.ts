291341c987784a0ba3fcef1bc9d2211aa80e6ed7
cyprain-okeke <108260115+cyprain-okeke@users.noreply.github.com>
2025-05-30 15:34:29 +0100

    Changes the revoke message (#14963)

--- apps/web/src/app/billing/members/free-bitwarden-families.component.ts
+++ apps/web/src/app/billing/members/free-bitwarden-families.component.ts
@@ -160,7 +160,7 @@ export class FreeBitwardenFamiliesComponent implements OnInit {
   private async doRevokeSponsorship(sponsorship: OrganizationSponsorshipInvitesResponse) {
     const content = sponsorship.validUntil
       ? this.i18nService.t(
-          "updatedRevokeSponsorshipConfirmationForAcceptedSponsorship",
+          "revokeActiveSponsorshipConfirmation",
           sponsorship.friendlyName,
           formatDate(sponsorship.validUntil, "MM/dd/yyyy", this.locale),
         )
