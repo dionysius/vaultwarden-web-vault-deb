1256f37ef0fb47d591998ca5f2db75b0d5176c4a
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-06-11 07:30:24 +0200

    disable seat limit check

--- apps/web/src/app/admin-console/organizations/members/members.component.ts
+++ apps/web/src/app/admin-console/organizations/members/members.component.ts
@@ -82,7 +82,6 @@ import {
   MemberDialogTab,
   openUserAddEditDialog,
 } from "./components/member-dialog";
-import { isFixedSeatPlan } from "./components/member-dialog/validators/org-seat-limit-reached.validator";
 import {
   ResetPasswordComponent,
   ResetPasswordDialogResult,
@@ -545,6 +544,7 @@ export class MembersComponent extends BaseMembersComponent<OrganizationUserView>
   }
 
   async invite() {
+    /* vaultwarden has no seat limit
     if (this.organization.hasReseller && this.organization.seats === this.occupiedSeatCount) {
       this.toastService.showToast({
         variant: "error",
@@ -562,7 +562,7 @@ export class MembersComponent extends BaseMembersComponent<OrganizationUserView>
       await this.handleSeatLimitForFixedTiers();
 
       return;
-    }
+    } end of ignore seat check */
 
     await this.handleInviteDialog();
   }
