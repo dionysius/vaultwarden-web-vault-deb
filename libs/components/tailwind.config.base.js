f9d0e6fe4a89834c458d59cfcb1029fcf54462e5
Jason Ng <jng@bitwarden.com>
2025-06-30 15:10:01 -0400

    [CL-572] adding new colors for icons, update icon docs (#15367)
    * adding new colors for icons, update icon docs, remove art from colors mdx

--- libs/components/tailwind.config.base.js
+++ libs/components/tailwind.config.base.js
@@ -63,6 +63,7 @@ module.exports = {
         100: rgba("--color-notification-100"),
         600: rgba("--color-notification-600"),
       },
+      // art styles deprecated, use 'illustration' instead
       art: {
         primary: rgba("--color-art-primary"),
         accent: rgba("--color-art-accent"),
@@ -83,6 +84,14 @@ module.exports = {
         alt4: rgba("--color-background-alt4"),
       },
       "marketing-logo": rgba("--color-marketing-logo"),
+      illustration: {
+        outline: rgba("--color-illustration-outline"),
+        "bg-primary": rgba("--color-illustration-bg-primary"),
+        "bg-secondary": rgba("--color-illustration-bg-secondary"),
+        "bg-tertiary": rgba("--color-illustration-bg-tertiary"),
+        tertiary: rgba("--color-illustration-tertiary"),
+        logo: rgba("--color-illustration-logo"),
+      },
     },
     textColor: {
       main: rgba("--color-text-main"),
