72d6eb3d995f914e46927dd10b90bfa46ae65202
Jason Ng <jng@bitwarden.com>
2025-07-15 12:27:24 -0400

    [PM-23199] improve search speed for large web vault (#15466)

--- apps/web/src/app/vault/individual-vault/vault.component.ts
+++ apps/web/src/app/vault/individual-vault/vault.component.ts
@@ -16,6 +16,7 @@ import {
   catchError,
   concatMap,
   debounceTime,
+  distinctUntilChanged,
   filter,
   first,
   map,
@@ -334,7 +335,11 @@ export class VaultComponent implements OnInit, OnDestroy {
     );
 
     this.searchText$
-      .pipe(debounceTime(SearchTextDebounceInterval), takeUntil(this.destroy$))
+      .pipe(
+        debounceTime(SearchTextDebounceInterval),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$),
+      )
       .subscribe((searchText) =>
         this.router.navigate([], {
           queryParams: { search: Utils.isNullOrEmpty(searchText) ? null : searchText },
