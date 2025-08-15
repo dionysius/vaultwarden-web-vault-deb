59169fb0c7c301859a1b7f7dcb12235cb972861f
Daniel Riera <driera@livefront.com>
2025-08-15 12:16:52 -0400

    PM-23976 Autofilling not working in https://www.sketchup.com (#15990)
    * PM-23976
    
    * add space

--- apps/browser/src/autofill/services/autofill.service.ts
+++ apps/browser/src/autofill/services/autofill.service.ts
@@ -2459,22 +2459,23 @@ export default class AutofillService implements AutofillServiceInterface {
         break;
       }
 
+      const includesUsernameFieldName =
+        this.findMatchingFieldIndex(f, AutoFillConstants.UsernameFieldNames) > -1;
+
       if (
         !f.disabled &&
         (canBeReadOnly || !f.readonly) &&
-        (withoutForm || f.form === passwordField.form) &&
+        (withoutForm || f.form === passwordField.form || includesUsernameFieldName) &&
         (canBeHidden || f.viewable) &&
         (f.type === "text" || f.type === "email" || f.type === "tel")
       ) {
         usernameField = f;
-
-        if (this.findMatchingFieldIndex(f, AutoFillConstants.UsernameFieldNames) > -1) {
-          // We found an exact match. No need to keep looking.
+        // We found an exact match. No need to keep looking.
+        if (includesUsernameFieldName) {
           break;
         }
       }
     }
-
     return usernameField;
   }
 
