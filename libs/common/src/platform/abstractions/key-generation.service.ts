1463fc804d439c27c10c931cbbc294f19c2da2d4
Bernd Schoolmann <mail@quexten.com>
2025-08-21 09:45:49 +0200

    Remove platform keygen service re-exports (#16081)

--- libs/common/src/platform/abstractions/key-generation.service.ts
+++
@@ -1,2 +0,0 @@
-/** Temporary re-export. This should not be used for new imports */
-export { KeyGenerationService } from "../../key-management/crypto/key-generation/key-generation.service";
--- libs/common/src/platform/services/key-generation.service.ts
+++
@@ -1,2 +0,0 @@
-/** Temporary re-export. This should not be used for new imports */
-export { DefaultKeyGenerationService as KeyGenerationService } from "../../key-management/crypto/key-generation/default-key-generation.service";
