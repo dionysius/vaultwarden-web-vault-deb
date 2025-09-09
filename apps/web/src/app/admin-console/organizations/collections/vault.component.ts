15619c626547fb3cb38cf9c114910ce079d47e7e
Brandon Treston <btreston@bitwarden.com>
2025-09-09 10:13:21 -0400

    add missing ? (#16356)

--- apps/web/src/app/admin-console/organizations/collections/vault.component.ts
+++ apps/web/src/app/admin-console/organizations/collections/vault.component.ts
@@ -1462,7 +1462,7 @@ export class vNextVaultComponent implements OnInit, OnDestroy {
         ciphers: items,
         organizationId: organization.id,
         availableCollections,
-        activeCollection: this.activeFilter.selectedCollectionNode.node,
+        activeCollection: this.activeFilter?.selectedCollectionNode?.node,
         isSingleCipherAdmin:
           items.length === 1 && (organization.canEditAllCiphers || items[0].isUnassigned),
       },
