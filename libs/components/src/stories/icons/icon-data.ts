45efd3e5abecd01b86e0133ccfa7f07676e702be
Vijay Oommen <voommen@livefront.com>
2025-09-09 10:01:05 -0500

    [PM-19996] added new icon for access-intelligence (#16315)

--- libs/components/src/stories/icons/icon-data.ts
+++ libs/components/src/stories/icons/icon-data.ts
@@ -46,6 +46,10 @@ const bitwardenObjects = [
     usage: "card item type",
   },
   {
+    id: "bwi-dashboard",
+    usage: "access-intelligence dashboard view",
+  },
+  {
     id: "bwi-family",
     usage: "family vault or organization",
   },
