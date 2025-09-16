befc756a022c61a3f6838ef7552c89a0aa681d0e
Vijay Oommen <voommen@livefront.com>
2025-09-16 08:58:01 -0500

    [PM-25694] [Defect] Secrets manager + AC integrations page not listing any integrations (#16401)

--- bitwarden_license/bit-web/src/app/secrets-manager/integrations/integrations.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/integrations/integrations.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from "@angular/core";
 
+import { HecOrganizationIntegrationService } from "@bitwarden/bit-common/dirt/organization-integrations/services/hec-organization-integration-service";
+import { OrganizationIntegrationApiService } from "@bitwarden/bit-common/dirt/organization-integrations/services/organization-integration-api.service";
+import { OrganizationIntegrationConfigurationApiService } from "@bitwarden/bit-common/dirt/organization-integrations/services/organization-integration-configuration-api.service";
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
+import { safeProvider } from "@bitwarden/ui-common";
+
 import { IntegrationCardComponent } from "../../dirt/organization-integrations/integration-card/integration-card.component";
 import { IntegrationGridComponent } from "../../dirt/organization-integrations/integration-grid/integration-grid.component";
 import { SecretsManagerSharedModule } from "../shared/sm-shared.module";
@@ -13,6 +19,23 @@ import { IntegrationsComponent } from "./integrations.component";
     IntegrationsRoutingModule,
     IntegrationCardComponent,
     IntegrationGridComponent,
+  ],
+  providers: [
+    safeProvider({
+      provide: HecOrganizationIntegrationService,
+      useClass: HecOrganizationIntegrationService,
+      deps: [OrganizationIntegrationApiService, OrganizationIntegrationConfigurationApiService],
+    }),
+    safeProvider({
+      provide: OrganizationIntegrationApiService,
+      useClass: OrganizationIntegrationApiService,
+      deps: [ApiService],
+    }),
+    safeProvider({
+      provide: OrganizationIntegrationConfigurationApiService,
+      useClass: OrganizationIntegrationConfigurationApiService,
+      deps: [ApiService],
+    }),
   ],
   declarations: [IntegrationsComponent],
 })
