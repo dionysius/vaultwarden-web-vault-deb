0e23d6d9f1485e3aac1cd98b58dc267ffb0574fb
Jonathan Prusik <jprusik@users.noreply.github.com>
2025-09-17 10:43:53 -0400

    add label-right to the list of checked field attributes when fuzzy matching (#16451)

--- apps/browser/src/autofill/services/autofill.service.ts
+++ apps/browser/src/autofill/services/autofill.service.ts
@@ -2513,6 +2513,7 @@ export default class AutofillService implements AutofillServiceInterface {
       "label-tag",
       "placeholder",
       "label-left",
+      "label-right",
       "label-top",
       "label-aria",
       "dataSetValues",
@@ -2609,7 +2610,7 @@ export default class AutofillService implements AutofillServiceInterface {
   }
 
   /**
-   * Updates a fill script to place the `cilck_on_opid`, `focus_on_opid`, and `fill_by_opid`
+   * Updates a fill script to place the `click_on_opid`, `focus_on_opid`, and `fill_by_opid`
    * fill script actions associated with the provided field.
    * @param {AutofillScript} fillScript
    * @param {AutofillField} field
