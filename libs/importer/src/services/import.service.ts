bcc050d4de8f10994d9812732354af6aa326752f
Oscar Hinton <Hinton@users.noreply.github.com>
2025-10-07 09:17:07 +0200

    [PM-26488][PM-26494] Disable chrome loader on MAS (#16699)
    * Disable chrome loader on MAS
    
    * Remove direct dependency on platform utils service in favour of the system
    
    * Move check above brave condition
    
    * Invert condition

--- libs/importer/src/services/import.service.ts
+++ libs/importer/src/services/import.service.ts
@@ -146,7 +146,8 @@ export class ImportService implements ImportServiceAbstraction {
       map(([type, enabled]) => {
         let loaders = availableLoaders(type, client);
 
-        let isUnsupported = false;
+        // Mac App Store is currently disabled due to sandboxing.
+        let isUnsupported = this.system.environment.isMacAppStore();
 
         if (enabled && type === "bravecsv") {
           try {
