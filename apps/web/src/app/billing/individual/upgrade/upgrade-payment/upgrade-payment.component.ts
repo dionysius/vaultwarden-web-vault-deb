9c7d794cac7060050cae51cd18e2bd1652413096
Stephon Brown <sbrown@livefront.com>
2025-10-10 12:05:27 -0400

    Billing/pm 26686/update upgrade account component to be responsive (#16808)
    * fix(billing): Update initialization of payment component
    
    * fix(billing): Add more signal validation
    
    * fix(billing): Add responsiveness to upgrade-account component

--- apps/web/src/app/billing/individual/upgrade/upgrade-payment/upgrade-payment.component.ts
+++ apps/web/src/app/billing/individual/upgrade/upgrade-payment/upgrade-payment.component.ts
@@ -118,26 +118,23 @@ export class UpgradePaymentComponent implements OnInit, AfterViewInit {
           tier: this.selectedPlanId(),
           details: planDetails,
         };
+        this.passwordManager = {
+          name: this.isFamiliesPlan ? "familiesMembership" : "premiumMembership",
+          cost: this.selectedPlan.details.passwordManager.annualPrice,
+          quantity: 1,
+          cadence: "year",
+        };
+
+        this.upgradeToMessage = this.i18nService.t(
+          this.isFamiliesPlan ? "upgradeToFamilies" : "upgradeToPremium",
+        );
+
+        this.estimatedTax = 0;
+      } else {
+        this.complete.emit({ status: UpgradePaymentStatus.Closed, organizationId: null });
+        return;
       }
     });
-
-    if (!this.selectedPlan) {
-      this.complete.emit({ status: UpgradePaymentStatus.Closed, organizationId: null });
-      return;
-    }
-
-    this.passwordManager = {
-      name: this.isFamiliesPlan ? "familiesMembership" : "premiumMembership",
-      cost: this.selectedPlan.details.passwordManager.annualPrice,
-      quantity: 1,
-      cadence: "year",
-    };
-
-    this.upgradeToMessage = this.i18nService.t(
-      this.isFamiliesPlan ? "upgradeToFamilies" : "upgradeToPremium",
-    );
-
-    this.estimatedTax = 0;
 
     this.formGroup.valueChanges
       .pipe(debounceTime(1000), takeUntilDestroyed(this.destroyRef))
@@ -146,7 +143,9 @@ export class UpgradePaymentComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.cartSummaryComponent.isExpanded.set(false);
+    if (this.cartSummaryComponent) {
+      this.cartSummaryComponent.isExpanded.set(false);
+    }
   }
 
   protected get isPremiumPlan(): boolean {
