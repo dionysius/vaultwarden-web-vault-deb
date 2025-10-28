fe26826369f65cca5480fc15fe38688a252e4cf1
Daniel Riera <driera@livefront.com>
2025-10-28 14:47:49 -0400

    PM-27366 drop scss and convert to vanilla css (#17046)

--- apps/browser/src/autofill/overlay/inline-menu/pages/button/bootstrap-autofill-inline-menu-button.ts
+++ apps/browser/src/autofill/overlay/inline-menu/pages/button/bootstrap-autofill-inline-menu-button.ts
@@ -1,10 +1,7 @@
 import { AutofillOverlayElement } from "../../../../enums/autofill-overlay.enum";
 
 import { AutofillInlineMenuButton } from "./autofill-inline-menu-button";
-
-// FIXME: Remove when updating file. Eslint update
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require("./button.scss");
+import "./button.css";
 
 (function () {
   globalThis.customElements.define(AutofillOverlayElement.Button, AutofillInlineMenuButton);
