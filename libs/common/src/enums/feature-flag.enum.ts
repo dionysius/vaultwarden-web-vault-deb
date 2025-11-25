17ae78ea8315e5b266d7ff9af393c61848859825
Vincent Salucci <26154748+vincentsalucci@users.noreply.github.com>
2025-11-25 11:23:01 -0600

    chore: fix feature flag name, refs PM-27766 (#17660)

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -13,7 +13,7 @@ export enum FeatureFlag {
   /* Admin Console Team */
   CreateDefaultLocation = "pm-19467-create-default-location",
   AutoConfirm = "pm-19934-auto-confirm-organization-users",
-  BlockClaimedDomainAccountCreation = "block-claimed-domain-account-creation",
+  BlockClaimedDomainAccountCreation = "pm-28297-block-uninvited-claimed-domain-registration",
 
   /* Auth */
   PM23801_PrefetchPasswordPrelogin = "pm-23801-prefetch-password-prelogin",
