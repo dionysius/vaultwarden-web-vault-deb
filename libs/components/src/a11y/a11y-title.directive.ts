dc953b3945b0fa383c714b35bb3254b37dfc9ccb
Bryan Cunningham <bcunningham@bitwarden.com>
2025-12-02 16:03:06 -0500

    Revert using tooltip in appA11yTitle directive (#17787)
    * revert using tooltip in title directive
    
    * add back tooltip delay from revert
    
    * add back label to carousel buttons
    
    * fix documentation that does not need reverted
    
    * remove unnecessary label attr

--- libs/components/src/a11y/a11y-title.directive.ts
+++ libs/components/src/a11y/a11y-title.directive.ts
@@ -1,38 +1,22 @@
-import { Directive, effect, ElementRef, inject } from "@angular/core";
-
-import { TooltipDirective } from "../tooltip/tooltip.directive";
+import { Directive, effect, ElementRef, input } from "@angular/core";
 
 import { setA11yTitleAndAriaLabel } from "./set-a11y-title-and-aria-label";
 
-/**
- * @deprecated This function is deprecated in favor of `bitTooltip`.
- * Please use `bitTooltip` instead.
- *
- * Directive that provides accessible tooltips by internally using TooltipDirective.
- * This maintains the appA11yTitle API while leveraging the enhanced tooltip functionality.
- */
 @Directive({
   selector: "[appA11yTitle]",
-  hostDirectives: [
-    {
-      directive: TooltipDirective,
-      inputs: ["bitTooltip: appA11yTitle", "tooltipPosition"],
-    },
-  ],
 })
 export class A11yTitleDirective {
-  private readonly elementRef = inject(ElementRef<HTMLElement>);
-  private readonly tooltipDirective = inject(TooltipDirective);
+  readonly title = input.required<string>({ alias: "appA11yTitle" });
 
-  constructor() {
-    const originalAriaLabel = this.elementRef.nativeElement.getAttribute("aria-label");
+  constructor(private el: ElementRef) {
+    const originalTitle = this.el.nativeElement.getAttribute("title");
+    const originalAriaLabel = this.el.nativeElement.getAttribute("aria-label");
 
-    // setting aria-label as a workaround for testing purposes. Should be removed once tests are updated to check element content.
     effect(() => {
       setA11yTitleAndAriaLabel({
-        element: this.elementRef.nativeElement,
-        title: undefined,
-        label: originalAriaLabel ?? this.tooltipDirective.tooltipContent(),
+        element: this.el.nativeElement,
+        title: originalTitle ?? this.title(),
+        label: originalAriaLabel ?? this.title(),
       });
     });
   }
