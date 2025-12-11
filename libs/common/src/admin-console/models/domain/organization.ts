cc8cb941058fea67e525e6075ff13fc1f4aa924e
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-12-11 22:01:38 +0100

    disable access intelligence

--- libs/common/src/admin-console/models/domain/organization.ts
+++ libs/common/src/admin-console/models/domain/organization.ts
@@ -403,6 +403,6 @@ export class Organization {
   }
 
   get canUseAccessIntelligence() {
-    return this.productTierType === ProductTierType.Enterprise;
+    return false; // no access intelligence in Vaultwarden
   }
 }
